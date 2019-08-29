7fa32429544181f22d780ba495fe8379e4446924
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2019-08-29 17:06:40 +0200

    adding dataset creator

--- mnist_cnn/create_mnist_fft.py
+++ mnist_cnn/create_mnist_fft.py
@@ -5,17 +5,18 @@ import h5py
 from skimage.transform import resize
 
 
-# Load MNIST dataset
-path = './data/mnist/'
-with gzip.open(path+'mnist.pkl.gz', 'rb') as f:
-    ((train_x, train_y), (valid_x, valid_y), _) = pickle.load(f, encoding='latin-1')
+def open_mnist(path):
+    with gzip.open(path, 'rb') as f:
+        ((train_x, _), (valid_x, _), _) = pickle.load(f, encoding='latin-1')
+    return train_x, valid_x
+
 
 def process_img(image):
     ''' Resize images to 64 x 64 and calculate fft 
         Return images as vector
     '''
     img_reshaped = image.reshape(28, 28)
-    img_rescaled = resize(img_reshaped, (64, 64))
+    img_rescaled = resize(img_reshaped, (64, 64), anti_aliasing=True, mode='constant')
     img_fft = np.fft.fftshift(np.fft.fft2(img_rescaled))
     return img_rescaled.reshape(4096), img_fft.reshape(4096)
 
@@ -24,19 +25,33 @@ def reshape_img(img, size=64):
     return img.reshape(size, size)
 
 
-# Process train images, split into x and y
-all_train = np.concatenate([process_img(img) for img in train_x]) 
-y_train = all_train[0::2]
-x_train = all_train[1::2]
+def write_h5(path, x, y):
+    with h5py.File(path, 'w') as hf:
+        hf.create_dataset(str(x),  data=x)
+        hf.create_dataset(str(y),  data=y)
+        hf.close()
 
-# Process valid images, split into x and y
-all_valid = np.concatenate([process_img(img) for img in valid_x]) 
-y_valid = all_valid[0::2]
-x_valid = all_valid[1::2]
+def main():
+    # Load MNIST dataset
+    path = './data/mnist/mnist.pkl.gz'
+    train_x, valid_x = open_mnist(path)
 
-# Write data to h5 file
-with h5py.File('data/mnist.h5', 'w') as hf:
-    hf.create_dataset('x_train',  data=x_train)
-    hf.create_dataset('y_train',  data=y_train)
-    hf.create_dataset('x_valid',  data=x_valid)
-    hf.create_dataset('y_valid',  data=y_valid)
+    # Process train images, split into x and y
+    all_train = np.concatenate([process_img(img) for img in train_x]) 
+    y_train = all_train[0::2]
+    x_train = all_train[1::2]
+
+    # Process valid images, split into x and y
+    all_valid = np.concatenate([process_img(img) for img in valid_x]) 
+    y_valid = all_valid[0::2]
+    x_valid = all_valid[1::2]
+
+    # Write data to h5 file
+    outpath_train = 'data/mnist_train.h5'
+    write_h5(outpath_train, x_train, y_train)
+    outpath_test = 'data/mnist_train.h5'
+    write_h5(outpath_test, x_valid, y_valid)
+
+
+if __name__ == '__main__':
+    main()
