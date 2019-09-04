d03a563c9af628a4fffad482bca53138c9142b0f
Kevin Schmidt <kevin3.schmidt@udo.edu>
2019-09-04 12:07:48 +0200

    fixing h5 column names

--- mnist_cnn/create_mnist_fft.py
+++ mnist_cnn/create_mnist_fft.py
@@ -25,10 +25,10 @@ def reshape_img(img, size=64):
     return img.reshape(size, size)
 
 
-def write_h5(path, x, y):
+def write_h5(path, x, y, name_x, name_y):
     with h5py.File(path, 'w') as hf:
-        hf.create_dataset(str(x),  data=x)
-        hf.create_dataset(str(y),  data=y)
+        hf.create_dataset(name_x,  data=x)
+        hf.create_dataset(name_y,  data=y)
         hf.close()
 
 def main():
@@ -48,9 +48,9 @@ def main():
 
     # Write data to h5 file
     outpath_train = 'data/mnist_train.h5'
-    write_h5(outpath_train, x_train, y_train)
+    write_h5(outpath_train, x_train, y_train, 'x_train', 'y_train')
     outpath_valid = 'data/mnist_valid.h5'
-    write_h5(outpath_valid, x_valid, y_valid)
+    write_h5(outpath_valid, x_valid, y_valid, 'x_valid', 'y_valid')
 
 
 if __name__ == '__main__':
