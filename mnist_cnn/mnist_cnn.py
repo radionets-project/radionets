d8f5e05686bf4982defb24581bb39ba87bee0af8
Kevin Schmidt <kevin3.schmidt@udo.edu>
2019-09-04 15:48:41 +0200

    adding small version of cnn model

--- mnist_cnn/mnist_cnn.py
+++ mnist_cnn/mnist_cnn.py
@@ -26,6 +26,10 @@ import torch.optim as optim
 from functools import partial
 
 from preprocessing import get_h5_data, prepare_dataset, get_dls, DataBunch
+from model import *
+from training import view_tfm, sched_no, Recorder, AvgStatsCallback, ParamScheduler, BatchTransformXCallback, CudaCallback, Runner
+from training import model_summary
+from training import find_lr
 
 # +
 # Load train and valid data
@@ -40,8 +44,66 @@ train_ds, valid_ds = prepare_dataset(x_train, y_train, x_valid, y_valid, log=Tru
 # Create databunch with definde batchsize
 bs = 128
 data = DataBunch(*get_dls(train_ds, valid_ds, bs), c=train_ds.c)
+# + {}
+# Define cuda device else cpu
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Define loss function
+loss_func = nn.MSELoss()
+
+# Define model
+def get_model(data, lr=1e-1):
+    model = nn.Sequential(
+        *conv(1, 4, (3,3), 1),
+        *conv(4, 8, (3,3), 1),
+        nn.MaxPool2d((3,3)),
+        Lambda(flatten),
+        nn.Linear(3200, data.c)
+    ).to(device)
+    return model, optim.SGD(model.parameters(), lr=lr)
+
+
+# +
+# Combine model and data in learner
+learn = Learner(*get_model(data), loss_func, data)
+
+# Initialize convolutional layers
+init_cnn(learn.model)
 # -
 
 
+# Find learning rate
+find_lr(learn)
 
+# +
+# Define resize for mnist data
+mnist_view = view_tfm(1, 64, 64)
 
+# Define schedueled learning rate
+sched = sched_no(3e-2, 3e-2)
+
+# Define callback functions
+cbfs = [
+    Recorder,
+    partial(AvgStatsCallback, loss_func),
+    partial(ParamScheduler, 'lr', sched),
+    CudaCallback,
+    partial(BatchTransformXCallback, mnist_view),
+]
+
+# Define runner
+run = Runner(cb_funcs=cbfs)
+# -
+
+# Show model summary
+model_summary(run, learn, data)
+
+# Train model
+run.fit(2500, learn)
+
+# Evaluate model
+evaluate(valid_ds, learn.model)
+
+# Save model
+state = learn.model.state_dict()
+torch.save(state, './mnist_cnn_small_1')
