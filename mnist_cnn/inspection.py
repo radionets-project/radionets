dacd2483da9523658e0bfc5b83caa79e1b469856
Kevin Schmidt <kevin3.schmidt@udo.edu>
2019-09-05 11:15:08 +0200

    fix cuda memory

--- mnist_cnn/inspection.py
+++ mnist_cnn/inspection.py
@@ -14,22 +14,22 @@ def training_stats(run):
     plt.tight_layout()
 
 def evaluate(valid_ds, model):
-    x_t = valid_ds.x.float().cuda()
-    h = np.sqrt(x_t.shape[1]).astype(int)
-    x_t = x_t.view(-1, h, h).unsqueeze(1)
+    x_t = valid_ds.x.float()
+    rand = np.random.randint(0, len(x_t))
+    img = x_t[rand].cuda()
+    h = int(np.sqrt(img.shape))
+    img = img.view(-1, h, h).unsqueeze(1)
     model.eval()
     with torch.no_grad():
-        pred = model(x_t).cpu()
+        pred = model(img).cpu()
 
-    img = np.random.randint(0, len(pred))
-    print('Image:', img)
     fig, (ax0, ax1, ax2, cax) = plt.subplots(ncols=4, figsize=(18, 6), gridspec_kw={"width_ratios":[1,1,1, 0.05]})
     ax0.set_title('x')
-    ax0.imshow(valid_ds.x[img].view(h, h))
+    ax0.imshow(img.view(h, h).cpu())
     ax1.set_title('y_pred')
-    im = ax1.imshow(pred[img].view(h, h), vmin=0, vmax=1)
+    im = ax1.imshow(pred.view(h, h), vmin=0, vmax=1)
     ax2.set_title('y_true')
-    ax2.imshow(valid_ds.y[img].view(h, h), vmin=0, vmax=1)
+    ax2.imshow(valid_ds.y[rand].view(h, h), vmin=0, vmax=1)
     fig.colorbar(im, cax=cax)
     plt.tight_layout()
 
