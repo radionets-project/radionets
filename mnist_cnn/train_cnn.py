767cd232c582ae34885962cf5791766321d309c3
Kevin Schmidt <kevin3.schmidt@udo.edu>
2019-09-17 15:58:49 +0200

    ready to train pretrained model

--- mnist_cnn/train_cnn.py
+++ mnist_cnn/train_cnn.py
@@ -3,10 +3,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from functools import partial
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 from preprocessing import get_h5_data, prepare_dataset, get_dls, DataBunch
 from model import conv, init_cnn, Learner, Lambda, flatten
 from training import view_tfm, sched_no, Recorder, AvgStatsCallback, ParamScheduler,\
-                        BatchTransformXCallback, CudaCallback, Runner
+                        BatchTransformXCallback, CudaCallback, Runner, SaveCallback
 from inspection import evaluate_model
 
 
@@ -20,79 +22,83 @@ from IPython import embed
 @click.argument('lr', type=float)
 @click.option('-log', type=bool, required=False)
 @click.option('-quantile', type=bool, required=False)
+@click.option('-positive', type=bool, required=False)
 @click.option('-pretrained', type=bool, required=False)
 @click.option('-inspection', type=bool, required=False)
 @click.argument('pretrained_model', type=click.Path(exists=True, dir_okay=True), required=False)
-def main(train_path, valid_path, model_path, num_epochs, lr, log=True, quantile=False,
+def main(train_path, valid_path, model_path, num_epochs, lr, log=True, quantile=False, positive=False,
          pretrained=False, pretrained_model=None, inspection=False):
     # Load data
     x_train, y_train = get_h5_data(train_path, columns=['x_train', 'y_train'])
     x_valid, y_valid = get_h5_data(valid_path, columns=['x_valid', 'y_valid'])
-    embed()
+
     # Create train and valid datasets
-    train_ds, valid_ds = prepare_dataset(x_train, y_train, x_valid, y_valid, log=log, quantile=quantile)
+    train_ds, valid_ds = prepare_dataset(x_train, y_train, x_valid, y_valid,
+                                         log=log, quantile=quantile, positive=positive)
 
-    # # Create databunch with definde batchsize
-    # bs = 128
-    # data = DataBunch(*get_dls(train_ds, valid_ds, bs), c=train_ds.c)
+    # Create databunch with definde batchsize
+    bs = 128
+    data = DataBunch(*get_dls(train_ds, valid_ds, bs), c=train_ds.c)
 
-    # # Define loss function
-    # loss_func = nn.MSELoss()
+    # Define loss function
+    loss_func = nn.MSELoss()
 
-    # # Define model
-    # def get_model(data, lr=1e-1): #1e-1
-    #     model = nn.Sequential(
-    #         *conv(1, 4, (3,3), 1),
-    #         *conv(4, 8, (3,3), 1),
-    #         *conv(8, 16, (3,3), 1),
-    #         nn.MaxPool2d((3,3)),
-    #         *conv(16, 32, (2,2), 1),
-    #         *conv(32, 64, (2,2), 1),
-    #         nn.MaxPool2d((2,2)),
-    #         Lambda(flatten),
-    #         nn.Linear(4096, data.c)
-    #     )
-    #     return model, optim.SGD(model.parameters(), lr=lr)
+    # Define model
+    def get_model(data, lr=1e-1): #1e-1
+        model = nn.Sequential(
+            *conv(1, 4, (3,3), 1),
+            *conv(4, 8, (3,3), 1),
+            *conv(8, 16, (3,3), 1),
+            nn.MaxPool2d((3,3)),
+            *conv(16, 32, (2,2), 1),
+            *conv(32, 64, (2,2), 1),
+            nn.MaxPool2d((2,2)),
+            Lambda(flatten),
+            nn.Linear(4096, data.c)
+        )
+        return model, optim.SGD(model.parameters(), lr=lr)
 
-    # # Combine model and data in learner
-    # learn = Learner(*get_model(data), loss_func, data)
+    # Combine model and data in learner
+    learn = Learner(*get_model(data), loss_func, data)
 
-    # if pretrained is True:
-    #     # Load model
-    #     m = learn.model
-    #     m.load_state_dict((torch.load(pretrained_model)))
-    # else:
-    #     # Initialize convolutional layers
-    #     init_cnn(learn.model)
+    if pretrained is True:
+        # Load model
+        print('Load pretrained model.')
+        m = learn.model
+        m.load_state_dict((torch.load(pretrained_model)))
+    else:
+        # Initialize convolutional layers
+        init_cnn(learn.model)
 
-    # # Define resize for mnist data
-    # mnist_view = view_tfm(1, 64, 64)
+    # Define resize for mnist data
+    mnist_view = view_tfm(1, 64, 64)
 
-    # # Define schedueled learning rate
-    # sched = sched_no(lr, lr)
+    # Define schedueled learning rate
+    sched = sched_no(lr, lr)
 
-    # # Define callback functions
-    # cbfs = [
-    #     Recorder,
-    #     partial(AvgStatsCallback, loss_func),
-    #     partial(ParamScheduler, 'lr', sched),
-    #     CudaCallback,
-    #     partial(BatchTransformXCallback, mnist_view),
-    # ]
+    # Define callback functions
+    cbfs = [
+        Recorder,
+        partial(AvgStatsCallback, loss_func),
+        partial(ParamScheduler, 'lr', sched),
+        CudaCallback,
+        partial(BatchTransformXCallback, mnist_view),
+        SaveCallback,
+    ]
 
-    # # Define runner
-    # run = Runner(cb_funcs=cbfs)
+    # Define runner
+    run = Runner(cb_funcs=cbfs)
 
-    # # Train model
-    # run.fit(num_epochs, learn)
+    # Train model
+    run.fit(num_epochs, learn)
 
-    # # Save model
-    # state = learn.model.state_dict()
-    # torch.save(state, model_path)
+    # Save model
+    state = learn.model.state_dict()
+    torch.save(state, model_path)
 
-    # if inspection is True:
-    #     evaluate_model(valid_ds, learn.model)
-    #     plt.savefig('inspection_plot.pdf', dpi=300, bbox_inches='tight', pad_inches=0.01)
+    if inspection is True:
+        evaluate_model(valid_ds, learn.model)
+        plt.savefig('inspection_plot.pdf', dpi=300, bbox_inches='tight', pad_inches=0.01)
 
 if __name__ == '__main__':
     main()
