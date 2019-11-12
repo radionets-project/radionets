7f9da014a8b534d4e487b416e7aaf3186b58df2e
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2019-11-12 18:02:37 +0100

    add pandas to requirements

--- setup.py
+++ setup.py
@@ -25,7 +25,8 @@ setup(
         'jupytext==1.2.4',
         'obspy==1.1.1',
         'h5py==2.9.0',
-        'scikit-image==0.16.2'
+        'scikit-image==0.16.2',
+        'pandas==0.25.3'
     ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
