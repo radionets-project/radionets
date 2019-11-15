fix normalization test

tests/test_mnist_normalization.py
@@ -6,6 +6,7 @@ def test_normalization():
     import numpy as np
     import os
     import pandas as pd
+    # from mnist_cnn.utils import create_mask
     train_path = 'mnist_cnn/data/mnist_samp_train.h5'
     valid_path = 'mnist_cnn/data/mnist_samp_valid.h5'
     normalization = 'mnist_cnn/data/normalization_factors.csv'
@@ -13,9 +14,16 @@ def test_normalization():
                                    & os.path.exists(normalization)):
         norm_values = pd.read_csv(normalization)
         train_mean = norm_values['train_mean'].values
+        valid_mean = norm_values['valid_mean'].values
         train_std = norm_values['train_std'].values
-        x_train, _ = get_h5_data(train_path, columns=['x_train', 'y_train'])
-        x_valid, _ = get_h5_data(valid_path, columns=['x_valid', 'y_valid'])
+        x_train, _ = np.log(get_h5_data(train_path, columns=['x_train', 'y_train']))
+        x_valid, _ = np.log(get_h5_data(valid_path, columns=['x_valid', 'y_valid']))
+
+        x_train[np.isinf(x_train)] = train_mean
+        x_valid[np.isinf(x_valid)] = valid_mean
+
+        assert not np.isinf(x_train).any()
+        assert not np.isinf(x_valid).any()
 
         x_train = normalize(x_train, train_mean, train_std)
         x_valid = normalize(x_valid, train_mean, train_std)