cf186fe7977e3c3ece204fc1ff50928eeb97f254
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2019-11-19 11:48:50 +0100

    pep8

--- sampling/source_simulations.py
+++ sampling/source_simulations.py
@@ -12,13 +12,14 @@ def gaussian_component(x, y, flux, x_fwhm, y_fwhm, rotation=0, center=None):
     else:
         x_0 = center[0]
         y_0 = center[1]
-        
+
     rotation = np.deg2rad(rotation)
     x_rot = (np.cos(rotation) * x - np.sin(rotation) * y) 
     y_rot = (np.sin(rotation) * x + np.cos(rotation) * y) 
-    
+
     gauss = flux * np.exp(- ((x_rot - x_0)**2/(2*(x_fwhm)**2) + (y_rot - y_0)**2/(2*(y_fwhm)**2)) )
     return gauss
+
 
 def create_grid(pixel):
     x = np.linspace(0, pixel-1, num=pixel)
@@ -26,21 +27,23 @@ def create_grid(pixel):
     X, Y = np.meshgrid(x, y)
     return X, Y
 
+
 def gaussian_source(X, Y, blur=False):
     source = np.zeros(X.shape)
     cent = len(X)//2
-    
+
     center = np.array([[cent, cent], [cent+10, cent], [cent-10, cent], [cent+20, cent], [cent-20, cent]])
     intens = np.array([5, 2, 2, 1, 1])
     fwhm = np.array([2, 4, 4, 6, 6])
-    
+
     for i in range(0,5):
         source += gaussian_component(X, Y, intens[i], fwhm[i], fwhm[i], center=center[i])
-    
+
     if blur is True:
         source = gaussian_filter(source, sigma=3)
-    
+
     return source
+
 
 def simulate_gaussian_source(pixel, blur=False):
     X, Y = create_grid(pixel)
