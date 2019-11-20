23120ff5f74df9675b81ad420e41993cd0b916f2
Felix Geyer <felix.geyer@tu-dortmund.de>
2019-11-20 15:05:05 +0100

    Outsourced two functions

--- mnist_cnn/visualize/utils.py
+++ mnist_cnn/visualize/utils.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import torch
-import imp
 
 
 def plot_mnist(img):
@@ -9,18 +8,6 @@ def plot_mnist(img):
     plt.xlabel('Pixel')
     plt.ylabel('Pixel')
     plt.tight_layout()
-
-
-def load_pre_model(model, pretrained_path):
-    model.load_state_dict(torch.load(pretrained_path))
-    print('Pretrained model loaded and put to cuda.')
-    return model
-
-
-def load_architecture(arch_path):
-    module = imp.load_source('get_model', arch_path)
-    model = module.get_model()
-    return model
 
 
 def eval_model(img, model):
