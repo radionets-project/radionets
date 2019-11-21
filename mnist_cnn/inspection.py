Added correct normalisation and get_eval_img for two channels
especially which input picture to view. Currently its the amplitude.
Also changed scale of colorbar in inspection plot.

mnist_cnn/inspection.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import torch
 from dl_framework.callbacks import view_tfm
 import pandas as pd
-from mnist_cnn.utils import normalize
+from dl_framework.data import normalize
 
 
 def training_stats(run):
@@ -17,13 +17,15 @@ def training_stats(run):
 
 def get_normalization(img, norm_path):
     norm = pd.read_csv(norm_path)
-    train_mean = torch.tensor(norm['train_mean'].values[0]).float()
-    train_std = torch.tensor(norm['train_std'].values[0]).float()
-    img_mean = img[~torch.isinf(img)].mean()
-    img[torch.isinf(img)] = img_mean
-    img_normed = normalize(img, train_mean, train_std)
-    assert not torch.isinf(img_normed).any()
-    return img_normed
+    train_mean_real = torch.tensor(norm['train_mean_real'].values[0]).float()
+    train_std_real = torch.tensor(norm['train_std_real'].values[0]).float()
+    train_mean_imag = torch.tensor(norm['train_mean_imag'].values[0]).float()
+    train_std_imag = torch.tensor(norm['train_std_imag'].values[0]).float()
+
+    img[:, 0] = normalize(img[:, 0], train_mean_real, train_std_real)
+    img[:, 1] = normalize(img[:, 1], train_mean_imag, train_std_imag)
+    # assert not torch.isinf().any()
+    return img
 
 
 def get_eval_img(valid_ds, model, norm_path):
@@ -31,8 +33,8 @@ def get_eval_img(valid_ds, model, norm_path):
     rand = np.random.randint(0, len(x_t))
     img = x_t[rand].cuda()
     img = get_normalization(img, norm_path)
-    h = int(np.sqrt(img.shape))
-    img = img.view(-1, h, h).unsqueeze(1)
+    h = int(np.sqrt(img.shape[1]))
+    img = img.view(-1, h, h).unsqueeze(0)
     model.eval()
     with torch.no_grad():
         pred = model(img).cpu()
@@ -46,11 +48,11 @@ def evaluate_model(valid_ds, model, norm_path, nrows=3):
     for i in range(nrows):
         img, pred, h, rand = get_eval_img(valid_ds, model, norm_path)
         axes[i][0].set_title('x')
-        axes[i][0].imshow(img.view(h, h).cpu(), cmap='RdGy_r',
+        axes[i][0].imshow(img[:, 0].view(h, h).cpu(), cmap='RdGy_r',
                           vmax=img.max(), vmin=-img.max())
         axes[i][1].set_title('y_pred')
-        im = axes[i][1].imshow(pred.view(h, h), vmin=valid_ds.y[rand].min(),
-                               vmax=valid_ds.y[rand].max())
+        im = axes[i][1].imshow(pred.view(h, h), vmin=pred.min(),
+                               vmax=pred.max())
         axes[i][2].set_title('y_true')
         axes[i][2].imshow(valid_ds.y[rand].view(h, h),
                           vmin=valid_ds.y[rand].min(),