4153a6eb954afff1ff626efc96da7583b1f247c9
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2019-11-27 15:42:53 +0100

    del lognorm

--- sampling/uv_plots.py
+++ sampling/uv_plots.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
 import cartopy.crs as ccrs
 from matplotlib.animation import FuncAnimation
 from matplotlib.animation import PillowWriter
