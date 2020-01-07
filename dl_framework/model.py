1de34db612ffeed5203ebf36d5b89877a1bc283c
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-01-07 17:56:28 +0100

    pep8 + adding fft layer

--- dl_framework/model.py
+++ dl_framework/model.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from math import sqrt
+import numpy as np
 
 
 class Lambda(nn.Module):
@@ -13,6 +14,11 @@ class Lambda(nn.Module):
         return self.func(x)
 
 
+def fft(x):
+    a = np.fft.fftshift(np.fft.ifft(x))
+    return a
+
+
 def flatten(x):
     a = x.view(x.shape[0], -1)
     return a
@@ -21,20 +27,26 @@ def flatten(x):
 class GeneralRelu(nn.Module):
     def __init__(self, leak=None, sub=None, maxv=None):
         super().__init__()
-        self.leak,self.sub,self.maxv = leak,sub,maxv
+        self.leak = leak
+        self.sub = sub
+        self.maxv = maxv
 
     def forward(self, x):
-        x = F.leaky_relu(x,self.leak) if self.leak is not None else F.relu(x)
-        if self.sub is not None: x.sub_(self.sub)
-        if self.maxv is not None: x.clamp_max_(self.maxv)
+        x = F.leaky_relu(x, self.leak) if self.leak is not None else F.relu(x)
+        if self.sub is not None:
+            x.sub_(self.sub)
+        if self.maxv is not None:
+            x.clamp_max_(self.maxv)
         return x
 
 
 def init_cnn_(m, f):
     if isinstance(m, nn.Conv2d):
         f(m.weight, a=0.1)
-        if getattr(m, 'bias', None) is not None: m.bias.data.zero_()
-    for l in m.children(): init_cnn_(l, f)
+        if getattr(m, 'bias', None) is not None:
+            m.bias.data.zero_()
+    for l in m.children():
+        init_cnn_(l, f)
 
 
 def init_cnn(m, uniform=False):
@@ -45,7 +57,8 @@ def init_cnn(m, uniform=False):
 def conv_layer(ni, nf, ks=3, stride=2, bn=True, **kwargs):
     layers = [nn.Conv2d(ni, nf, ks, padding=ks//2, stride=stride, bias=not bn),
               GeneralRelu(**kwargs)]
-    if bn: layers.append(nn.BatchNorm2d(nf, eps=1e-5, momentum=0.1))
+    if bn:
+        layers.append(nn.BatchNorm2d(nf, eps=1e-5, momentum=0.1))
     return nn.Sequential(*layers)
 
 
@@ -53,43 +66,45 @@ class RunningBatchNorm(nn.Module):
     def __init__(self, nf, mom=0.1, eps=1e-5):
         super().__init__()
         self.mom, self.eps = mom, eps
-        self.mults = nn.Parameter(torch.ones (nf,1,1))
-        self.adds  = nn.Parameter(torch.zeros(nf,1,1))
-        self.register_buffer('sums', torch.zeros(1,nf,1,1))
-        self.register_buffer('sqrs', torch.zeros(1,nf,1,1))
+        self.mults = nn.Parameter(torch.ones(nf, 1, 1))
+        self.adds = nn.Parameter(torch.zeros(nf, 1, 1))
+        self.register_buffer('sums', torch.zeros(1, nf, 1, 1))
+        self.register_buffer('sqrs', torch.zeros(1, nf, 1, 1))
         self.register_buffer('count', torch.tensor(0.))
         self.register_buffer('factor', torch.tensor(0.))
         self.register_buffer('offset', torch.tensor(0.))
         self.batch = 0
 
     def update_stats(self, x):
-        bs,nc,*_ = x.shape
+        bs, nc, *_ = x.shape
         self.sums.detach_()
         self.sqrs.detach_()
-        dims = (0,2,3)
-        s    = x    .sum(dims, keepdim=True)
-        ss   = (x*x).sum(dims, keepdim=True)
-        c    = s.new_tensor(x.numel()/nc)
+        dims = (0, 2, 3)
+        s = x.sum(dims, keepdim=True)
+        ss = (x*x).sum(dims, keepdim=True)
+        c = s.new_tensor(x.numel()/nc)
         mom1 = s.new_tensor(1 - (1-self.mom)/sqrt(bs-1))
-        self.sums .lerp_(s , mom1)
+        self.sums .lerp_(s, mom1)
         self.sqrs .lerp_(ss, mom1)
-        self.count.lerp_(c , mom1)
+        self.count.lerp_(c, mom1)
         self.batch += bs
         means = self.sums/self.count
         varns = (self.sqrs/self.count).sub_(means*means)
-        if bool(self.batch < 20): varns.clamp_min_(0.01)
+        if bool(self.batch < 20):
+            varns.clamp_min_(0.01)
         self.factor = self.mults / (varns+self.eps).sqrt()
         self.offset = self.adds - means*self.factor
 
     def forward(self, x):
-        if self.training: self.update_stats(x)
+        if self.training:
+            self.update_stats(x)
         return x*self.factor + self.offset
 
 
 def conv(ni, nc, ks, stride, padding):
     conv = nn.Conv2d(ni, nc, ks, stride, padding),
     bn = nn.BatchNorm2d(nc),
-    act = GeneralRelu(leak=0.1, sub=0.4) # nn.ReLU()
+    act = GeneralRelu(leak=0.1, sub=0.4)  # nn.ReLU()
     layers = [*conv, *bn, act]
     return layers
 
