0b5aa9f183e64a4d2ed04009f03aa7106caf42c9
Kevin Laudamus Kevin.Laudamus@studium.uni-hamburg.de <p1wy030@lgc2.lofar.gpu.cluster>
2020-01-14 14:11:33 +0100

    deleting unnecessery file

--- hello.py
+++
@@ -1,7 +0,0 @@
-import torch
-from mnist_cnn.utils import process_img
-from sampling.uv_simulations import sample_freqs
-from dl_framework.architectures import cnn
-from dl_framework.utils import listify
-
-print('Hello')
