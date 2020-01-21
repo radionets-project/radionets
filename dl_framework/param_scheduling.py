2e9cd292717084bb0d9633372cf4a94d56749f5d
Felix Geyer <felix.geyer@tu-dortmund.de>
2020-01-21 10:39:36 +0100

    Changed to flake8 conform form

--- dl_framework/param_scheduling.py
+++ dl_framework/param_scheduling.py
@@ -1,32 +1,48 @@
-from functools import partial
 import math
-from dl_framework.utils import listify
-import torch
+from functools import partial
 
+import torch
+from dl_framework.utils import listify
 
 # learning rate schedule schemes
 
+
 def annealer(f):
-    def _inner(start, end): return partial(f, start, end)
+    def _inner(start, end):
+        return partial(f, start, end)
+
     return _inner
 
+
 @annealer
-def sched_lin(start, end, pos): return start + pos*(end-start)
+def sched_lin(start, end, pos):
+    return start + pos * (end - start)
+
+
 @annealer
-def sched_cos(start, end, pos): return start + (1 + math.cos(math.pi*(1-pos))) * (end-start) / 2
+def sched_cos(start, end, pos):
+    return start + (1 + math.cos(math.pi * (1 - pos))) * (end - start) / 2
+
+
 @annealer
-def sched_no(start, end, pos):  return start
+def sched_no(start, end, pos):
+    return start
+
+
 @annealer
-def sched_exp(start, end, pos): return start * (end/start) ** pos
+def sched_exp(start, end, pos):
+    return start * (end / start) ** pos
 
 
 def combine_scheds(pcts, scheds):
-    assert sum(pcts) == 1.
+    assert sum(pcts) == 1.0
     pcts = torch.tensor([0] + listify(pcts))
     assert torch.all(pcts >= 0)
     pcts = torch.cumsum(pcts, 0)
+
     def _inner(pos):
         idx = (pos >= pcts).nonzero().max()
-        actual_pos = (pos-pcts[idx]) / (pcts[idx+1]-pcts[idx])
+        actual_pos = (pos - pcts[idx]) / (pcts[idx + 1] - pcts[idx])
         return scheds[idx](actual_pos)
+
     return _inner
