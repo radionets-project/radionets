9510931e8e8f992115f76a08c2451e813a70b8f2
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-01-23 18:20:23 +0100

    seems rdy to train

--- gaussian_sources/fft_tests.py
+++ gaussian_sources/fft_tests.py
@@ -20,17 +20,33 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import re
 
-path = 'data/'
-bundles = get_bundles(path)
+data_path = 'data/'
+bundles = get_bundles(data_path)
 bundles = [path for path in bundles if re.findall('gaussian_sources', path.name)]
 bundles
 
+# +
 # %%time
-for path in bundles:
+samp = True
+specific_mask = True
+lon = -80
+lat = 50
+steps = 50
+antenna_config_path = '../simulations/layouts/vlba.txt'
+out_path = 'data/fft_samp'
+
+for path in bundles[0:1]:
     bundle = open_bundle(path)
     bundle_fft = np.array([np.fft.fftshift(np.fft.fft2(img)) for img in bundle])
-    out = 'data/fft' + path.name.split('_')[-1]
-    save_fft_pair(out, bundle_fft, bundle)
+    if samp is True:
+        if specific_mask is True:
+            bundle_fft = np.array([sample_freqs(img, antenna_config_path, 128, lon, lat, steps)
+                                   for img in tqdm(bundle_fft)])
+        else:
+            bundle_fft = np.array([sample_freqs(img, antenna_config_path, size=128)
+                                   for img in tqdm(bundle_fft)])
+    out = out_path + path.name.split('_')[-1]
+    # save_fft_pair(out, bundle_fft, bundle)
     print(path)
     print(bundle.shape)
     print(bundle_fft.shape)
@@ -48,34 +64,173 @@ ax2.imshow(np.abs(bundle_fft[i]))
 
 ax3 = plt.subplot(333)
 ax3.imshow(np.angle(bundle_fft[i]))
+# -
+data_path = 'data/'
+bundle_paths = get_bundles(data_path)
+bundle_paths = [path for path in bundle_paths if re.findall('fft_samp', path.name)]
+bundle_paths
 
 # +
-# %%time
-specific_mask = True
-lon = -80
-lat = 50
-steps = 50
-antenna_config_path = '../simulations/layouts/vlba.txt'
+from gaussian_sources.preprocessing import split_amp_phase, mean_and_std
+from dl_framework.data import open_fft_pair
 
-if specific_mask is True:
-    bundle_fft_samp = np.array([sample_freqs(img, antenna_config_path, 128, lon, lat, steps)
-                                for img in tqdm(bundle_fft)])
-else:
-    bundle_fft_samp = np.array([sample_freqs(img, antenna_config_path, size=128)
-                                for img in tqdm(bundle_fft)])
+means_amp = np.array([])
+stds_amp = np.array([])
+means_phase = np.array([])
+stds_phase = np.array([])
+
+for path in tqdm(bundle_paths):
+    x, _ = open_fft_pair(path)
+    x_amp, x_phase = split_amp_phase(x)
+    mean_amp, std_amp = mean_and_std(x_amp)
+    mean_phase, std_phase = mean_and_std(x_phase)
+    means_amp = np.append(mean_amp, means_amp)
+    means_phase = np.append(mean_phase, means_phase)
+    stds_amp = np.append(std_amp, stds_amp)
+    stds_phase = np.append(std_phase, stds_phase)
+
+mean_amp = means_amp.mean()
+std_amp = stds_amp.mean()
+mean_phase = means_phase.mean()
+std_phase = stds_phase.mean()
+
+
+d = {'train_mean_amp': [mean_amp],
+     'train_std_amp': [std_amp],
+     'train_mean_phase': [mean_phase],
+     'train_std_phase': [std_phase]
+     }
+# -
+
+d
+
+bundle_paths
+
+from dl_framework.data import DataBunch, get_dls, h5_dataset
+import h5py
+import torch
+
+
+class h5_dataset():
+    def __init__(self, bundle_paths):
+        self.bundles = bundle_paths
+
+    def __call__(self):
+        return print('This is the h5_dataset class.')
+
+    def __len__(self):
+        return len(self.bundles) * len(self.open_bundle(self.bundles[0], 'x'))
+
+    def __getitem__(self, i):
+        x = self.open_image('x', i)
+        y = self.open_image('y', i)
+        return x, y
+
+    def open_bundle(self, bundle_path, var):
+        bundle = h5py.File(bundle_path, 'r')
+        data = bundle[str(var)]
+        return data
+
+    def open_image(self, var, i):
+        bundle_i = i // 1024
+        image_i = i - bundle_i * 1024
+        bundle = h5py.File(self.bundles[bundle_i], 'r')
+        data = bundle[str(var)][image_i]
+        if var == 'x':
+            data_amp, data_phase = split_amp_phase(data)
+            data_channel = combine_and_swap_axes(data_amp, data_phase)
+        else:
+            data_channel = data
+        return torch.tensor(data_channel).float().reshape(-1,16384)
+
+
+
+def combine_and_swap_axes(array1, array2):
+    return np.swapaxes(np.dstack((array1, array2)), 2, 0)
+
+
+from gaussian_sources.preprocessing import split_amp_phase
+'''
+def prepare_dataset(x_train, y_train, x_valid, y_valid):
+    """ Preprocessing dataset:
+    split
+    log (optional)
+    create ArrayDataset
+    """
+
+    x_train_amp, x_train_phase = split_amp_phase(x_train)
+    x_valid_amp, x_valid_phase = split_amp_phase(x_valid)
+
+    x_train = combine_and_swap_axes(x_train_amp, x_train_phase)
+    x_valid = combine_and_swap_axes(x_valid_amp, x_valid_phase)
+
+    x_train, y_train, x_valid, y_valid = map(torch.tensor, (x_train, y_train,
+                                                            x_valid, y_valid))
+
+    train_ds = h5dataset(x_train, y_train)
+    valid_ds = h5dataset(x_valid, y_valid)
+
+    return train_ds, valid_ds
+'''
+
+train_ds = h5_dataset(bundle_paths)
+valid_ds = h5_dataset(bundle_paths)
+
+bs = 256
+data = DataBunch(*get_dls(train_ds, valid_ds, bs))
+
+data.train_ds[0][1].shape
+
+next(iter(data.train_dl))[0].shape
 
 # +
-i = 9
+import dl_framework.architectures as architecture
+from dl_framework.learner import get_learner
+from dl_framework.optimizer import StatefulOptimizer, weight_decay,\
+                                   AverageGrad
+from dl_framework.optimizer import adam_step, AverageSqrGrad, StepCount
+from dl_framework.param_scheduling import sched_no
+from dl_framework.callbacks import Recorder, AvgStatsCallback,\
+                                   BatchTransformXCallback, CudaCallback,\
+                                   SaveCallback, view_tfm, ParamScheduler,\
+                                   normalize_tfm
+from functools import partial
+import torch.nn as nn
 
-f = plt.figure(figsize=(10,10))
-ax1 = plt.subplot(331)
-ax1.imshow(np.abs(bundle[i]))
+# Define architecture
+arch = getattr(architecture, 'cnn')()
 
-ax2 = plt.subplot(332)
-ax2.imshow(np.abs(bundle_fft[i]))
+# Define resize for mnist data
+mnist_view = view_tfm(2, 128, 128)
 
-ax3 = plt.subplot(333)
-ax3.imshow(np.angle(bundle_fft[i]))
+# make normalisation
+norm = normalize_tfm('./data/normalization_factors.csv')
+
+
+# Define callback functions
+cbfs = [
+    Recorder,
+    # test for use of multiple Metrics or Loss functions
+    partial(AvgStatsCallback, metrics=[nn.MSELoss(), nn.L1Loss()]),
+    CudaCallback,
+    partial(BatchTransformXCallback, norm),
+    partial(BatchTransformXCallback, mnist_view),
+    SaveCallback,
+]
+
+# Define optimiser function
+adam_opt = partial(StatefulOptimizer, steppers=[adam_step, weight_decay],
+                   stats=[AverageGrad(dampening=True), AverageSqrGrad(),
+                   StepCount()])
+
+# Combine model and data in learner
+learn = get_learner(data, arch, 1e-3, opt_func=adam_opt,  cb_funcs=cbfs)
+
+# Print model architecture
+print(learn.model, '\n')
+
+# Train the model, make it possible to stop at any given time
+learn.fit(1)
 # -
 
 
@@ -83,3 +238,39 @@ ax3.imshow(np.angle(bundle_fft[i]))
 
 
 
+
+
+
+
+
+
+
+
+
+
+class h5_dataset():
+    def __init__(self, bundle_paths):
+        self.bundles = bundle_paths
+
+    def __call__(self):
+        return print('This is the h5_dataset class.')
+
+    def __len__(self):
+        return len(self.bundles) * len(self.open_bundle(self.bundles[0], 'x'))
+
+    def __getitem__(self, i):
+        x = self.open_image('x', i)
+        y = self.open_image('y', i)
+        return x, y
+
+    def open_bundle(self, bundle_path, var):
+        bundle = h5py.File(bundle_path, 'r')
+        data = bundle[str(var)]
+        return data
+
+    def open_image(self, var, i):
+        bundle_i = i // 1024
+        image_i = i - bundle_i * 1024
+        bundle = h5py.File(self.bundles[bundle_i], 'r')
+        data = bundle[str(var)][image_i]
+        return data
