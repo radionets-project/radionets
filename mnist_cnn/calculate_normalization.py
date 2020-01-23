b52860b89b44880e7a94d49d1d2b34c7e0b5fd89
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-01-23 15:15:55 +0100

    drop last log options

--- mnist_cnn/calculate_normalization.py
+++ mnist_cnn/calculate_normalization.py
@@ -8,9 +8,7 @@ import pandas as pd
 @click.argument('train_path', type=click.Path(exists=True, dir_okay=True))
 @click.argument('valid_path', type=click.Path(exists=True, dir_okay=True))
 @click.argument('out_path', type=click.Path(exists=False, dir_okay=True))
-@click.option('-log', type=bool, required=False)
-@click.option('-use_mask', type=bool, required=False)
-def main(train_path, valid_path, out_path, log=False, use_mask=False):
+def main(train_path, valid_path, out_path):
     x_train, _ = get_h5_data(train_path, columns=['x_train', 'y_train'])
 
     # split in real and imaginary part
