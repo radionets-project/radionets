3209cb5272092ec2c3ae7388a9a78c4883e2949b
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-02-05 12:53:41 +0100

    fix naming

--- gaussian_sources/calculate_normalization.py
+++ gaussian_sources/calculate_normalization.py
@@ -18,31 +18,31 @@ def main(data_path, out_path):
     bundle_paths = [path for path in bundle_paths
                     if re.findall('fft_samp_train', path.name)]
 
-    means_amp = np.array([])
-    stds_amp = np.array([])
-    means_phase = np.array([])
-    stds_phase = np.array([])
+    means_real = np.array([])
+    stds_real = np.array([])
+    means_imag = np.array([])
+    stds_imag = np.array([])
 
     for path in tqdm(bundle_paths):
         x, _ = open_fft_pair(path)
         # split in amp and phase
-        x_amp, x_phase = split_real_imag(x)
-        mean_amp, std_amp = mean_and_std(x_amp)
-        mean_phase, std_phase = mean_and_std(x_phase)
-        means_amp = np.append(mean_amp, means_amp)
-        means_phase = np.append(mean_phase, means_phase)
-        stds_amp = np.append(std_amp, stds_amp)
-        stds_phase = np.append(std_phase, stds_phase)
+        x_real, x_imag = split_real_imag(x)
+        mean_real, std_real = mean_and_std(x_real)
+        mean_imag, std_imag = mean_and_std(x_imag)
+        means_real = np.append(mean_real, means_real)
+        means_imag = np.append(mean_imag, means_imag)
+        stds_real = np.append(std_real, stds_real)
+        stds_imag = np.append(std_imag, stds_imag)
 
-    mean_amp = means_amp.mean()
-    std_amp = stds_amp.mean()
-    mean_phase = means_phase.mean()
-    std_phase = stds_phase.mean()
+    mean_real = means_real.mean()
+    std_real = stds_real.mean()
+    mean_imag = means_imag.mean()
+    std_imag = stds_imag.mean()
 
-    d = {'train_mean_real': [mean_amp],
-         'train_std_real': [std_amp],
-         'train_mean_imag': [mean_phase],
-         'train_std_imag': [std_phase]
+    d = {'train_mean_real': [mean_real],
+         'train_std_real': [std_real],
+         'train_mean_imag': [mean_imag],
+         'train_std_imag': [std_imag]
          }
 
     df = pd.DataFrame(data=d)
