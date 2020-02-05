c4f089d9f11bdd0ad1ef16a313a160fae51cdf07
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-02-05 11:43:49 +0100

    weight decay

--- dl_framework/learner.py
+++ dl_framework/learner.py
@@ -73,7 +73,7 @@ class Learner():
         # NEW: pass callbacks to fit() and have them removed when done
         self.add_cbs(cbs)
         # NEW: create optimizer on fit(), optionally replacing existing
-        if reset_opt or not self.opt: self.opt = self.opt_func(self.splitter(self.model), lr=self.lr)
+        if reset_opt or not self.opt: self.opt = self.opt_func(self.splitter(self.model), lr=self.lr)# weight_decay=0.1)
 
         try:
             self.do_begin_fit(epochs)
