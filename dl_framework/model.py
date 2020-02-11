52691ae6ac36cfc13f4475ed97e5d1a4479732a1
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-02-11 10:32:23 +0100

    add fftshift

--- dl_framework/model.py
+++ dl_framework/model.py
@@ -15,13 +15,14 @@ class Lambda(nn.Module):
 
 def fft(x):
     img_size = x.size(1) // 2
-    # print(int(sqrt(img_size)))
     arr_real = x[:, 0:img_size].reshape(-1, int(sqrt(img_size)), int(sqrt(img_size)))
     arr_imag = x[:, img_size:].reshape(-1, int(sqrt(img_size)), int(sqrt(img_size)))
     arr = torch.stack((arr_real, arr_imag), dim=-1)
-    arr_fft = torch.ifft(arr, 2)
-    # print('FFt fertig')
-    return arr_fft.permute(0, 3, 1, 2)
+    arr_fft = torch.ifft(arr, 2).permute(0, 3, 2, 1).transpose(2, 3)
+    axes = tuple(range(arr_fft.ndim))
+    shift = [-(dim // 2) for dim in arr_fft.shape]
+    arr_shift = torch.roll(arr_fft, shift, axes)
+    return arr_shift
 
 
 def shape(x):
@@ -30,8 +31,7 @@ def shape(x):
 
 
 def flatten(x):
-    a = x.reshape(x.shape[0], -1)
-    return a
+    return x.reshape(x.shape[0], -1)
 
 
 def cut_off(x):
