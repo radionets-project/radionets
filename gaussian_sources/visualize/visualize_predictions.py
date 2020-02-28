08e8309ab80e87c0a4cfa442b12d942a43d50c92
Felix Geyer <felix.geyer@tu-dortmund.de>
2020-02-28 12:53:49 +0100

    Deleted unnecessary pretrained path and arch
    In visualize_predicitons, it is not necessary to load the architetcure
    anymore since the implementation of save_predictions

--- gaussian_sources/visualize/visualize_predictions.py
+++ gaussian_sources/visualize/visualize_predictions.py
@@ -2,8 +2,6 @@ import click
 import matplotlib
 import matplotlib.pyplot as plt
 import torch
-import dl_framework.architectures as architecture
-from dl_framework.model import load_pre_model
 from tqdm import tqdm
 from gaussian_sources.inspection import (
     visualize_without_fourier,
@@ -14,16 +12,12 @@ from gaussian_sources.inspection import (
 
 
 @click.command()
-@click.argument("arch", type=str)
-@click.argument("pretrained_path", type=click.Path(exists=True, dir_okay=True))
 @click.argument("out_path", type=click.Path(exists=False, dir_okay=True))
 @click.option("-fourier", type=bool, required=True)
 @click.option("-log", type=bool, required=False)
 @click.option("-index", type=int, required=False)
 @click.option("-num", type=int, required=False)
 def main(
-    arch,
-    pretrained_path,
     out_path,
     fourier,
     index=None,
@@ -49,12 +43,6 @@ def main(
         img_pred = imgs_pred[index]
         img_truth = imgs_truth[index]
 
-    # get arch
-    arch = getattr(architecture, arch)()
-
-    # load pretrained model
-    load_pre_model(arch, pretrained_path, visualize=True)
-
     if index is None:
         print("\nPlotting {} pictures.\n".format(num))
         for i in tqdm(range(len(indices))):
@@ -69,12 +57,12 @@ def main(
                     real_truth,
                     imag_truth,
                 ) = visualize_with_fourier(
-                    i, img_input, img_pred, img_truth, arch, out_path,
+                    i, img_input, img_pred, img_truth, out_path,
                 )
                 visualize_fft(i, real_pred, imag_pred, real_truth, imag_truth, out_path)
             else:
                 visualize_without_fourier(
-                    i, img_input, img_pred, img_truth, arch, out_path
+                    i, img_input, img_pred, img_truth, out_path
                 )
 
     else:
@@ -82,12 +70,12 @@ def main(
         i, index = 0, 0
         if fourier:
             real_pred, imag_pred, real_truth, imag_truth = visualize_with_fourier(
-                i, img_input, img_pred, img_truth, arch, out_path,
+                i, img_input, img_pred, img_truth, out_path,
             )
             visualize_fft(i, real_pred, imag_pred, real_truth, imag_truth, out_path)
         else:
             visualize_without_fourier(
-                i, img_input, img_pred, img_truth, arch, out_path,
+                i, img_input, img_pred, img_truth, out_path,
             )
     matplotlib.rcParams.update(matplotlib.rcParamsDefault)
 
