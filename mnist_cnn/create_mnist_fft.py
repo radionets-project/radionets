1d710ea250fd49745411bf64e1626a9701fe5f89
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-03-27 22:31:15 +0100

    creating fft bundles for train and valid

--- mnist_cnn/create_mnist_fft.py
+++ mnist_cnn/create_mnist_fft.py
@@ -1,51 +1,42 @@
 import click
-import numpy as np
 from tqdm import tqdm
-from utils import open_mnist, prepare_mnist, write_h5
+from utils import open_mnist, prepare_mnist_bundles
 
 
 @click.command()
-@click.argument('data_path', type=click.Path(exists=True, dir_okay=True))
-@click.argument('out_path', type=click.Path(exists=False, dir_okay=True))
-@click.option('-test', type=bool, required=False)
-@click.option('-noise', type=bool, required=False)
-def main(data_path, out_path, test=False, noise=False):
-    # Load MNIST dataset
+@click.argument("data_path", type=click.Path(exists=True, dir_okay=True))
+@click.argument("out_path", type=click.Path(exists=False, dir_okay=True))
+@click.option("-noise", type=bool, required=False)
+@click.option("-size", type=int, required=True)
+def main(data_path, out_path, size, noise=False):
+    """
+    Load MNIST dataset, split it into bundles, resize and compute fft.
+    Bundles are saved to hdf5 files.
+
+    Parameters
+    ----------
+    data_path: str
+        path to MNIST pickle file
+    out_path: str
+        directory where bundles are saved
+    noise: bool
+        if true: images are noised before fft
+    """
     path = data_path
     train_x, valid_x = open_mnist(path)
 
-    # Check if its a test call
-    # take only the first 50 pictures for a faster run
-    if test:
-        train_x = train_x[0:50]
-        valid_x = valid_x[0:50]
+    bundles_train = train_x.reshape(100, 500, 28, 28)
+    [
+        prepare_mnist_bundles(bund, out_path, "train", pixel=size, noise=noise)
+        for bund in tqdm(bundles_train[0:1])
+    ]
 
-    all_train = np.asarray([prepare_mnist(train_x[i])for i in range(len(train_x))])
-    all_train_swap = np.swapaxes(all_train, 1, 0)
-    x_train = all_train_swap[0]
-    y_train = all_train_swap[1]
-
-    all_valid = np.asarray([prepare_mnist(valid_x[i])for i in range(len(valid_x))])
-    all_valid_swap = np.swapaxes(all_valid, 1, 0)
-    x_valid = all_valid_swap[0]
-    y_valid = all_valid_swap[1]
-
-    # # Process train images, split into x and y
-    # all_train = np.concatenate([process_img(img, noise) for img in tqdm(train_x)])
-    # y_train = np.abs(all_train[0::2])
-    # x_train = all_train[1::2]
-
-    # # Process valid images, split into x and y
-    # all_valid = np.concatenate([process_img(img, noise) for img in tqdm(valid_x)])
-    # y_valid = np.abs(all_valid[0::2])
-    # x_valid = all_valid[1::2]
-
-    # # Write data to h5 file
-    # outpath_train = str(out_path) + '/mnist_train.h5'
-    # write_h5(outpath_train, x_train, y_train, 'x_train', 'y_train')
-    # outpath_valid = str(out_path) + '/mnist_valid.h5'
-    # write_h5(outpath_valid, x_valid, y_valid, 'x_valid', 'y_valid')
+    bundles_valid = valid_x.reshape(20, 500, 28, 28)
+    [
+        prepare_mnist_bundles(bund, out_path, "valid", pixel=size, noise=noise)
+        for bund in tqdm(bundles_valid[0:1])
+    ]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
