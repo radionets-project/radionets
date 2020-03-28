d8d9b02323283aa7dcedf492b00a76cc31db6c5f
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-03-28 20:30:09 +0100

    making bundle size variable

--- mnist_cnn/create_mnist_fft.py
+++ mnist_cnn/create_mnist_fft.py
@@ -1,14 +1,15 @@
 import click
 from tqdm import tqdm
-from utils import open_mnist, prepare_mnist_bundles
+from mnist_cnn.utils import open_mnist, prepare_mnist_bundles
 
 
 @click.command()
 @click.argument("data_path", type=click.Path(exists=True, dir_okay=True))
 @click.argument("out_path", type=click.Path(exists=False, dir_okay=True))
-@click.option("-noise", type=bool, required=False)
 @click.option("-size", type=int, required=True)
-def main(data_path, out_path, size, noise=False):
+@click.option("-bundle_size", type=int, required=True)
+@click.option("-noise", type=bool, required=False)
+def main(data_path, out_path, size, bundle_size, noise=False):
     """
     Load MNIST dataset, split it into bundles, resize and compute fft.
     Bundles are saved to hdf5 files.
@@ -22,26 +23,29 @@ def main(data_path, out_path, size, noise=False):
     noise: bool
         if true: images are noised before fft
     """
-    path = data_path
-    train_x, valid_x = open_mnist(path)
+    train_x, valid_x = open_mnist(data_path)
 
     print("\nCreating train set.\n")
-    bundles_train = train_x.reshape(100, 500, 28, 28)
+    bundles_train = train_x.reshape(
+        int(len(train_x) / bundle_size), bundle_size, 28, 28
+    )
     [
         prepare_mnist_bundles(bund, out_path, "train", pixel=size, noise=noise)
         for bund in tqdm(bundles_train[0:1])
     ]
 
     print("\nCreating valid set.\n")
-    bundles_evaluation = valid_x.reshape(20, 500, 28, 28)
-    bundles_valid = bundles_evaluation[0:18]
+    bundles_evaluation = valid_x.reshape(
+        int(len(valid_x) / bundle_size), bundle_size, 28, 28
+    )
+    bundles_valid = bundles_evaluation[0:-2]
     [
         prepare_mnist_bundles(bund, out_path, "valid", pixel=size, noise=noise)
         for bund in tqdm(bundles_valid[0:1])
     ]
 
     print("\nCreating test set.\n")
-    bundles_test = bundles_evaluation[18:20]
+    bundles_test = bundles_evaluation[-2:]
     [
         prepare_mnist_bundles(bund, out_path, "test", pixel=size, noise=noise)
         for bund in tqdm(bundles_test[0:1])
