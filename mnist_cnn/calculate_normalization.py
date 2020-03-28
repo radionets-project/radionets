cfb260d0d134e0e5e76637a8dc6ff99ee475f06c
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-03-28 18:40:49 +0100

    adjust normalization calculation for bundles

--- mnist_cnn/calculate_normalization.py
+++ mnist_cnn/calculate_normalization.py
@@ -1,28 +1,51 @@
 import click
-from mnist_cnn.utils import get_h5_data
-from mnist_cnn.utils import split_real_imag, mean_and_std
+import re
+import numpy as np
 import pandas as pd
+from tqdm import tqdm
+from dl_framework.data import split_real_imag, mean_and_std, get_bundles, open_fft_pair
 
 
 @click.command()
-@click.argument('train_path', type=click.Path(exists=True, dir_okay=True))
-@click.argument('out_path', type=click.Path(exists=False, dir_okay=True))
-def main(train_path, out_path):
-    x_train, _ = get_h5_data(train_path, columns=['x_train', 'y_train'])
+@click.argument("data_path", type=click.Path(exists=True, dir_okay=True))
+@click.argument("out_path", type=click.Path(exists=False, dir_okay=True))
+def main(data_path, out_path):
+    bundle_paths = get_bundles(data_path)
+    bundle_paths = [
+        path for path in bundle_paths if re.findall("fft_bundle_samp_train", path.name)
+    ]
 
-    # split in real and imaginary part
-    x_train_real, x_train_imag = split_real_imag(x_train)
+    means_real = np.array([])
+    stds_real = np.array([])
+    means_imag = np.array([])
+    stds_imag = np.array([])
 
-    train_mean_real, train_std_real = mean_and_std(x_train_real)
-    train_mean_imag, train_std_imag = mean_and_std(x_train_imag)
-    d = {'train_mean_real': [train_mean_real],
-         'train_std_real': [train_std_real],
-         'train_mean_imag': [train_mean_imag],
-         'train_std_imag': [train_std_imag]
-         }
+    for path in tqdm(bundle_paths):
+        x, _ = open_fft_pair(path)
+        # split in real and imaginary part
+        x_real, x_imag = split_real_imag(x)
+        mean_real, std_real = mean_and_std(x_real)
+        mean_imag, std_imag = mean_and_std(x_imag)
+        means_real = np.append(mean_real, means_real)
+        means_imag = np.append(mean_imag, means_imag)
+        stds_real = np.append(std_real, stds_real)
+        stds_imag = np.append(std_imag, stds_imag)
+
+    mean_real = means_real.mean()
+    std_real = stds_real.mean()
+    mean_imag = means_imag.mean()
+    std_imag = stds_imag.mean()
+
+    d = {
+        "train_mean_real": [mean_real],
+        "train_std_real": [std_real],
+        "train_mean_imag": [mean_imag],
+        "train_std_imag": [std_imag],
+    }
+
     df = pd.DataFrame(data=d)
     df.to_csv(out_path, index=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
