1bdf2ffa8bf8cb74ff286eff2ddff53dc178a52c
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-03-28 14:04:45 +0100

    adding test set

--- mnist_cnn/create_mnist_fft.py
+++ mnist_cnn/create_mnist_fft.py
@@ -25,16 +25,26 @@ def main(data_path, out_path, size, noise=False):
     path = data_path
     train_x, valid_x = open_mnist(path)
 
+    print("\nCreating train set.\n")
     bundles_train = train_x.reshape(100, 500, 28, 28)
     [
         prepare_mnist_bundles(bund, out_path, "train", pixel=size, noise=noise)
         for bund in tqdm(bundles_train[0:1])
     ]
 
-    bundles_valid = valid_x.reshape(20, 500, 28, 28)
+    print("\nCreating valid set.\n")
+    bundles_evaluation = valid_x.reshape(20, 500, 28, 28)
+    bundles_valid = bundles_evaluation[0:18]
     [
         prepare_mnist_bundles(bund, out_path, "valid", pixel=size, noise=noise)
         for bund in tqdm(bundles_valid[0:1])
+    ]
+
+    print("\nCreating test set.\n")
+    bundles_test = bundles_evaluation[18:20]
+    [
+        prepare_mnist_bundles(bund, out_path, "test", pixel=size, noise=noise)
+        for bund in tqdm(bundles_test[0:1])
     ]
 
 
