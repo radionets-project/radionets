651e5952f0c1db44e8065243987f07d37de08aaa
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-04-06 12:52:12 +0200

    fixing unet_denoise for 63 pixel images

--- dl_framework/architectures.py
+++ dl_framework/architectures.py
@@ -9,6 +9,7 @@ from dl_framework.model import (
     double_conv,
     cut_off,
     flatten_with_channel,
+    shape,
 )
 
 
@@ -135,7 +136,10 @@ class UNet_denoise(nn.Module):
         self.dconv_down5 = nn.Sequential(*double_conv(32, 64, (3, 3), 1, 1),)
 
         self.maxpool = nn.MaxPool2d(2)
-        self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+        self.upsample1 = nn.Upsample(size=7, mode="bilinear", align_corners=True)
+        self.upsample2 = nn.Upsample(size=15, mode="bilinear", align_corners=True)
+        self.upsample3 = nn.Upsample(size=31, mode="bilinear", align_corners=True)
+        self.upsample4 = nn.Upsample(size=63, mode="bilinear", align_corners=True)
 
         self.dconv_up4 = nn.Sequential(*double_conv(32 + 64, 32, (3, 3), 1, 1),)
         self.dconv_up3 = nn.Sequential(*double_conv(16 + 32, 16, (3, 3), 1, 1),)
@@ -144,9 +148,10 @@ class UNet_denoise(nn.Module):
 
         self.conv_last = nn.Conv2d(4, 2, 1)
         self.flatten = Lambda(flatten)
-        self.linear = nn.Linear(8192, 4096)
+        self.linear = nn.Linear(7938, 3969)
         self.fft = Lambda(fft)
         self.cut = Lambda(cut_off)
+        self.shape = Lambda(shape)
 
     def forward(self, x):
         x = self.flatten(x)
@@ -161,16 +166,16 @@ class UNet_denoise(nn.Module):
         x = self.maxpool(conv4)
         x = self.dconv_down5(x)
 
-        x = self.upsample(x)
+        x = self.upsample1(x)
         x = torch.cat([x, conv4], dim=1)
         x = self.dconv_up4(x)
-        x = self.upsample(x)
+        x = self.upsample2(x)
         x = torch.cat([x, conv3], dim=1)
         x = self.dconv_up3(x)
-        x = self.upsample(x)
+        x = self.upsample3(x)
         x = torch.cat([x, conv2], dim=1)
         x = self.dconv_up2(x)
-        x = self.upsample(x)
+        x = self.upsample4(x)
         x = torch.cat([x, conv1], dim=1)
         x = self.dconv_up1(x)
         x = self.conv_last(x)
