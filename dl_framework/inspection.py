2d5ca22e3b2a696f373d76786313e8d24f5f640b
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-04-08 17:03:13 +0200

    adding an inspection file to dl_framework with documentation

---
+++ dl_framework/inspection.py
@@ -0,0 +1,263 @@
+import re
+import torch
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from dl_framework.data import get_bundles, h5_dataset, do_normalisation
+import dl_framework.architectures as architecture
+from dl_framework.model import load_pre_model
+
+
+def load_eval_data(data_path, mode, fourier=False):
+    """
+    Load eval data set.
+
+    Parameters
+    ----------
+    data_path: str
+        path to data directory
+    mode: str
+        specify data set type, e.g. test
+    fourier: bool
+        use Fourier images as target if True, default is False
+
+    Returns
+    -------
+    test_ds: h5_dataset
+        dataset containing x and y images
+    """
+    bundle_paths = get_bundles(data_path)
+    test = [
+        path
+        for path in bundle_paths
+        if re.findall("fft_bundle_samp_" + mode, path.name)
+    ]
+    test_ds = h5_dataset(test, tar_fourier=fourier)
+    return test_ds
+
+
+def load_pretrained_model(arch_name, model_path):
+    """
+    Load model architecture and pretrained weigths.
+
+    Parameters
+    ----------
+    arch_name: str
+        name of the architecture (architectures are in dl_framework.architectures)
+    model_path: str
+        path to pretrained model
+
+    Returns
+    -------
+    arch: architecture object
+        architecture with pretrained weigths
+    """
+    arch = getattr(architecture, arch_name)()
+    load_pre_model(arch, model_path, visualize=True)
+    return arch
+
+
+def get_images(test_ds, num_images, norm_path):
+    """
+    Get n random test and truth images.
+
+    Parameters
+    ----------
+    test_ds: h5_dataset
+        data set with test images
+    num_images: int
+        number of test images
+    norm_path: str
+        path to normalization factors
+
+    Returns
+    -------
+    img_test: n 2d arrays
+        test images
+    img_true: n 2d arrays
+        truth images
+    """
+    rand = torch.randint(0, len(test_ds), size=(num_images,))
+    img_test = test_ds[rand][0]
+    norm = pd.read_csv(norm_path)
+    img_test = do_normalisation(img_test, norm)
+    img_true = test_ds[rand][1]
+    if num_images == 1:
+        img_test = img_test.unsqueeze(0)
+        img_true = img_true.unsqueeze(0)
+    return img_test, img_true
+
+
+def eval_model(img, model):
+    """
+    Put model into eval mode and evaluate test images.
+
+    Parameters
+    ----------
+    img: str
+        test image
+    model: architecture object
+        architecture with pretrained weigths
+
+    Returns
+    -------
+    pred: n 1d arrays
+        predicted images
+    """
+    model.eval()
+    with torch.no_grad():
+        pred = model(img.float())
+    return pred
+
+
+def reshape_2d(array):
+    """
+    Reshape 1d arrays into 2d ones.
+
+    Parameters
+    ----------
+    array: 1d array
+        input array
+
+    Returns
+    -------
+    array: 2d array
+        reshaped array
+    """
+    shape = [int(np.sqrt(array.shape[-1]))] * 2
+    return array.reshape(-1, *shape)
+
+
+def plot_results(inp, pred, truth):
+    """
+    Plot input images, prediction and true image.
+
+    Parameters
+    ----------
+    inp: n 2d arrays with 2 channel
+        input images
+    pred: n 2d arrays
+        predicted images
+    truth:n 2d arrays
+        true images
+    """
+    for i in range(len(inp)):
+        fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(10, 8))
+
+        real = inp[i][0]
+        im1 = ax1.imshow(real, cmap="RdBu", vmin=-real.max(), vmax=real.max())
+        divider = make_axes_locatable(ax1)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        ax1.set_title(r"Real Input")
+        fig.colorbar(im1, cax=cax, orientation="vertical")
+
+        imag = inp[i][1]
+        im2 = ax2.imshow(imag, cmap="RdBu", vmin=-imag.max(), vmax=imag.max())
+        divider = make_axes_locatable(ax2)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        ax2.set_title(r"Imag Input")
+        fig.colorbar(im2, cax=cax, orientation="vertical")
+
+        pre = pred[i]
+        im3 = ax3.imshow(pre, cmap="RdBu", vmin=-pre.max(), vmax=pre.max())
+        divider = make_axes_locatable(ax3)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        ax3.set_title(r"Prediction")
+        fig.colorbar(im3, cax=cax, orientation="vertical")
+
+        true = truth[i]
+        im4 = ax4.imshow(true, cmap="RdBu", vmin=-pre.max(), vmax=pre.max())
+        divider = make_axes_locatable(ax4)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        ax4.set_title(r"Truth")
+        fig.colorbar(im4, cax=cax, orientation="vertical")
+
+        plt.tight_layout()
+
+
+def plot_dataset(data, num_images):
+    """
+    Plot input and target images of a data set.
+
+    Parameters
+    ----------
+    data: h5_dataset
+        data set to be visualized
+    num_images: int
+        number of plotted images
+    """
+    for i in range(num_images):
+        fig, ((ax1, ax2, ax3)) = plt.subplots(1, 3, figsize=(10, 8))
+
+        inp1 = data[i][0][0]
+        im1 = ax1.imshow(inp1, cmap="RdBu", vmin=-inp1.max(), vmax=inp1.max())
+        divider = make_axes_locatable(ax1)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        ax1.set_title(r"Real Input")
+        fig.colorbar(im1, cax=cax, orientation="vertical")
+
+        inp2 = data[i][0][1]
+        im2 = ax2.imshow(inp2, cmap="RdBu", vmin=-inp2.max(), vmax=inp2.max())
+        divider = make_axes_locatable(ax2)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        ax2.set_title(r"Imag Input")
+        fig.colorbar(im2, cax=cax, orientation="vertical")
+
+        target = reshape_2d(data[i][1])[0]
+        im3 = ax3.imshow(target, cmap="RdBu", vmin=-target.max(), vmax=target.max())
+        divider = make_axes_locatable(ax3)
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        ax3.set_title(r"Target")
+        fig.colorbar(im3, cax=cax, orientation="vertical")
+
+        plt.tight_layout()
+
+
+def plot_loss(learn, model_path):
+    """
+    Plot train and valid loss of model.
+
+    Parameters
+    ----------
+    learn: learner-object
+        learner containing data and model
+    model_path: str
+        path to trained model
+    """
+    # to prevent the localhost error from happening first change the backende and
+    # second turn off the interactive mode
+    mpl.use("Agg")
+    plt.ioff()
+    name_model = model_path.split("/")[-1].split(".")[0]
+    save_path = model_path.split(".model")[0]
+    print("\nPlotting Loss for: {}\n".format(name_model))
+    learn.recorder.plot_loss()
+    plt.savefig("{}_loss.pdf".format(save_path), bbox_inches="tight", pad_inches=0.01)
+    mpl.rcParams.update(mpl.rcParamsDefault)
+
+
+def plot_lr_loss(learn, arch_name, out_path, skip_last):
+    """
+    Plot loss of learning rate finder.
+
+    Parameters
+    ----------
+    learn: learner-object
+        learner containing data and model
+    arch_path: str
+        name of the architecture
+    out_path: str
+        path to save loss plot
+    skip_last: int
+        skip n last points
+    """
+    # to prevent the localhost error from happening first change the backende and
+    # second turn off the interactive mode
+    mpl.use("Agg")
+    plt.ioff()
+    print("\nPlotting Lr vs Loss for architecture: {}\n".format(arch_name))
+    learn.recorder_lr_find.plot(skip_last, save=True)
+    plt.savefig(out_path + "/lr_loss.pdf", bbox_inches="tight", pad_inches=0.01)
+    mpl.rcParams.update(mpl.rcParamsDefault)
