c7a320a2caaf0c2daca65e6d8347241da8a43a1e
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-04-20 13:07:58 +0200

    sampling all data sets in one task

--- gaussian_sources/create_fft_pairs.py
+++ gaussian_sources/create_fft_pairs.py
@@ -14,29 +14,23 @@ import re
 
 
 @click.command()
-@click.argument("in_path", type=click.Path(exists=False, dir_okay=True))
+@click.argument("data_path", type=click.Path(exists=False, dir_okay=True))
 @click.argument("out_path", type=click.Path(exists=False, dir_okay=True))
 @click.argument("antenna_config_path", type=click.Path(exists=True, dir_okay=False))
-@click.option("-mode", type=str, required=True)
 @click.option("-amp_phase", type=bool, required=True)
 @click.option("-fourier", type=bool, required=True)
-@click.option("-samp", type=bool, required=False)
-@click.option("-size", type=int, required=True)
 @click.option("-specific_mask", type=bool)
 @click.option("-lon", type=float, required=False)
 @click.option("-lat", type=float, required=False)
 @click.option("-steps", type=float, required=False)
 @click.option("-noise", type=bool)
-@click.option("-preview", type=bool)
+@click.option("-preview", type=bool, required=False, default=False)
 def main(
-    in_path,
+    data_path,
     out_path,
     antenna_config_path,
-    mode,
     amp_phase,
     fourier,
-    size,
-    samp=True,
     specific_mask=False,
     lon=None,
     lat=None,
@@ -53,48 +47,62 @@ def main(
         save to new h5 file
     tagg train and valid in filename
     """
-    bundles = get_bundles(in_path)
-    bundles = [
-        path
-        for path in bundles
-        if re.findall("gaussian_sources_{}".format(mode), path.name)
-    ]
+    modes = ["train", "valid", "test"]
 
-    for path in tqdm(bundles):
-        bundle = open_bundle(path)
-        images = bundle.copy()
-        if noise is True:
-            images = add_noise(images, preview=preview)
-        bundle_fft = np.array([np.fft.fftshift(np.fft.fft2(img)) for img in images])
+    for mode in modes:
+        print(f"Sampling {mode} data set.")
+        bundles = get_bundles(data_path)
+        bundles = [
+            path
+            for path in bundles
+            if re.findall("gaussian_sources_{}".format(mode), path.name)
+        ]
 
-        if amp_phase:
-            amp, phase = split_amp_phase(bundle_fft)
-            amp = (np.log10(amp + 1e-10)/10) + 1
-            bundle_fft = np.stack((amp, phase), axis=1)
-        else:
-            real, imag = split_real_imag(bundle_fft)
-            bundle_fft = np.stack((real, imag), axis=1)
+        for path in tqdm(bundles):
+            bundle = open_bundle(path)
+            images = bundle.copy()
+            size = bundle.shape[-1]
+            if noise is True:
+                images = add_noise(images, preview=preview)
+            bundle_fft = np.array([np.fft.fftshift(np.fft.fft2(img)) for img in images])
 
-        copy = bundle_fft.copy()
-        if samp is True:
+            if amp_phase:
+                amp, phase = split_amp_phase(bundle_fft)
+                amp = (np.log10(amp + 1e-10) / 10) + 1
+                bundle_fft = np.stack((amp, phase), axis=1)
+            else:
+                real, imag = split_real_imag(bundle_fft)
+                bundle_fft = np.stack((real, imag), axis=1)
+
+            copy = bundle_fft.copy()
             if specific_mask is True:
                 bundle_samp = np.array(
                     [
                         sample_freqs(
-                            img, antenna_config_path, size, lon, lat, steps, plot=False, test=False,
+                            img,
+                            antenna_config_path,
+                            size,
+                            lon,
+                            lat,
+                            steps,
+                            plot=False,
+                            test=False,
                         )
                         for img in copy
                     ]
                 )
             else:
                 bundle_samp = np.array(
-                    [sample_freqs(img, antenna_config_path, size=size) for img in copy]
+                    [
+                        sample_freqs(img, antenna_config_path, size=size)
+                        for img in copy
+                    ]
                 )
-        out = out_path + path.name.split("_")[-1]
-        if fourier:
-            save_fft_pair(out, bundle_samp, bundle_fft)
-        else:
-            save_fft_pair(out, bundle_samp, images)
+            out = out_path + path.name.split("_")[-1]
+            if fourier:
+                save_fft_pair(out, bundle_samp, bundle_fft)
+            else:
+                save_fft_pair(out, bundle_samp, images)
 
 
 if __name__ == "__main__":
