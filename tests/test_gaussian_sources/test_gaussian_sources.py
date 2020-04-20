53df35e9acc3078938ae0e3640f6515f19bec248
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-04-20 13:21:52 +0200

    testing fft_samp for fourier and without fourier

--- tests/test_gaussian_sources/test_gaussian_sources.py
+++ tests/test_gaussian_sources/test_gaussian_sources.py
@@ -35,13 +35,44 @@ def test_create_fft_pairs():
     from gaussian_sources.create_fft_pairs import main
 
     data_path = "./tests/build/gaussian_sources"
-    out_path = "./tests/build/gaussian_sources"
+    out_path = "./tests/build/gaussian_sources/fourier"
+    Path(out_path).mkdir(parents=True, exist_ok=True)
     antenna_config = "./simulations/layouts/vlba.txt"
 
     runner = CliRunner()
     options = [
         data_path,
-        out_path,
+        out_path+"/fft_samp_",
+        antenna_config,
+        "-amp_phase",
+        True,
+        "-fourier",
+        True,
+        "-specific_mask",
+        True,
+        "-lon",
+        -80,
+        "-lat",
+        50,
+        "-steps",
+        50,
+        "-noise",
+        True,
+        "-preview",
+        False,
+    ]
+    result = runner.invoke(main, options)
+    print(traceback.print_exception(*result.exc_info))
+
+    assert result.exit_code == 0
+
+    out_path = "./tests/build/gaussian_sources/wo_fourier"
+    Path(out_path).mkdir(parents=True, exist_ok=True)
+
+    runner = CliRunner()
+    options = [
+        data_path,
+        out_path+"/fft_samp_",
         antenna_config,
         "-amp_phase",
         True,
@@ -58,7 +89,7 @@ def test_create_fft_pairs():
         "-noise",
         True,
         "-preview",
-        False
+        False,
     ]
     result = runner.invoke(main, options)
     print(traceback.print_exception(*result.exc_info))
