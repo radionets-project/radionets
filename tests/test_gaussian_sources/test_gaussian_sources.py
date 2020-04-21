01deff8c1bb7eebaf1cae829e2db67382bc383b6
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-04-21 11:02:53 +0200

    fixing calc_norm test for wo_fourier

--- tests/test_gaussian_sources/test_gaussian_sources.py
+++ tests/test_gaussian_sources/test_gaussian_sources.py
@@ -7,7 +7,7 @@ import traceback
 def test_simulate_bundles():
     from gaussian_sources.simulate_bundles import main
 
-    num_bundles_train = "2"
+    num_bundles_train = "1"
     num_bundles_valid = "1"
     num_bundles_test = "1"
     bundle_size = "5"
@@ -23,8 +23,6 @@ def test_simulate_bundles():
         bundle_size,
         img_size,
         out_path,
-        "-noise",
-        True,
     ]
     result = runner.invoke(main, options)
     print(traceback.print_exception(*result.exc_info))
@@ -76,7 +74,7 @@ def test_create_fft_pairs():
         out_path + "/fft_samp_",
         antenna_config,
         "-amp_phase",
-        True,
+        False,
         "-fourier",
         False,
         "-specific_mask",
@@ -98,54 +96,88 @@ def test_create_fft_pairs():
     assert result.exit_code == 0
 
 
-def test_normalization():
+def test_normalization_fourier():
     from gaussian_sources.calculate_normalization import main
     import pandas as pd
     from dl_framework.data import get_bundles, open_fft_pair, do_normalisation
     import re
     import torch
 
-    paths = [
-        "./tests/build/gaussian_sources/fourier",
-        "./tests/build/gaussian_sources/wo_fourier",
+    path = "./tests/build/gaussian_sources/fourier"
+
+    data_path = path
+    out_path = path + "/normalization_factors.csv"
+
+    runner = CliRunner()
+    options = [data_path, out_path]
+    result = runner.invoke(main, options)
+    print(traceback.print_exception(*result.exc_info))
+
+    assert result.exit_code == 0
+
+    factors = pd.read_csv(out_path)
+
+    assert (
+        factors.keys()
+        == ["train_mean_c0", "train_std_c0", "train_mean_c1", "train_std_c1", ]
+    ).all()
+    assert ~np.isnan(factors.values).all()
+    assert ~np.isinf(factors.values).all()
+    assert (factors.values != 0).all()
+
+    bundle_paths = get_bundles(data_path)
+    bundle_paths = [
+        path for path in bundle_paths if re.findall("fft_samp_train", path.name)
     ]
 
-    for path in paths:
-        data_path = path
-        out_path = path + "/normalization_factors.csv"
+    bundles = [open_fft_pair(bund) for bund in bundle_paths]
 
-        runner = CliRunner()
-        options = [data_path, out_path]
-        result = runner.invoke(main, options)
-        print(traceback.print_exception(*result.exc_info))
+    a = np.stack((bundles[0][0][:, 0], bundles[0][0][:, 1]), axis=1)
 
-        assert result.exit_code == 0
+    assert np.isclose(do_normalisation(torch.tensor(a), factors).mean(), 0, atol=1e-1)
+    assert np.isclose(do_normalisation(torch.tensor(a), factors).std(), 1, atol=1e-1)
 
-        factors = pd.read_csv(out_path)
 
-        assert (
-            factors.keys()
-            == ["train_mean_c0", "train_std_c0", "train_mean_c1", "train_std_c1", ]
-        ).all()
-        assert ~np.isnan(factors.values).all()
-        assert ~np.isinf(factors.values).all()
-        assert (factors.values != 0).all()
+def test_normalization_wo_fourier():
+    from gaussian_sources.calculate_normalization import main
+    import pandas as pd
+    from dl_framework.data import get_bundles, open_fft_pair, do_normalisation
+    import re
+    import torch
 
-        bundle_paths = get_bundles(data_path)
-        bundle_paths = [
-            path for path in bundle_paths if re.findall("fft_samp_train", path.name)
-        ]
+    path = "./tests/build/gaussian_sources/wo_fourier"
 
-        bundles = [open_fft_pair(bund) for bund in bundle_paths]
+    data_path = path
+    out_path = path + "/normalization_factors.csv"
 
-        a = np.stack((bundles[0][0][:, 0], bundles[0][0][:, 1]), axis=1)
+    runner = CliRunner()
+    options = [data_path, out_path]
+    result = runner.invoke(main, options)
+    print(traceback.print_exception(*result.exc_info))
 
-        assert np.isclose(
-            do_normalisation(torch.tensor(a), factors).mean(), 0, atol=1e-1
-        )
-        assert np.isclose(
-            do_normalisation(torch.tensor(a), factors).std(), 1, atol=1e-1
-        )
+    assert result.exit_code == 0
+
+    factors = pd.read_csv(out_path)
+
+    assert (
+        factors.keys()
+        == ["train_mean_c0", "train_std_c0", "train_mean_c1", "train_std_c1", ]
+    ).all()
+    assert ~np.isnan(factors.values).all()
+    assert ~np.isinf(factors.values).all()
+    assert (factors.values != 0).all()
+
+    bundle_paths = get_bundles(data_path)
+    bundle_paths = [
+        path for path in bundle_paths if re.findall("fft_samp_train", path.name)
+    ]
+
+    bundles = [open_fft_pair(bund) for bund in bundle_paths]
+
+    a = np.stack((bundles[0][0][:, 0], bundles[0][0][:, 1]), axis=1)
+
+    assert np.isclose(do_normalisation(torch.tensor(a), factors).mean(), 0, atol=1e-1)
+    assert np.isclose(do_normalisation(torch.tensor(a), factors).std(), 1, atol=1e-1)
 
 
 def test_train_cnn_fourier():
@@ -153,7 +185,7 @@ def test_train_cnn_fourier():
 
     data_path = "./tests/build/gaussian_sources/fourier"
     path_model = "./tests/build/gaussian_sources/fourier/test.model"
-    arch = "UNet_denoise"
+    arch = "UNet_fourier"
     norm_path = "./tests/build/gaussian_sources/fourier/normalization_factors.csv"
     epochs = "5"
     lr = "1e-3"
@@ -171,13 +203,54 @@ def test_train_cnn_fourier():
         lr_type,
         bs,
         "-fourier",
-        False,
+        True,
+        "-amp_phase",
+        True,
         "-pretrained",
         False,
         "-inspection",
         False,
-        # "-test",
-        # True,
+        "-test",
+        True,
+    ]
+    result = runner.invoke(main, options)
+    print(traceback.print_exception(*result.exc_info))
+
+    assert result.exit_code == 0
+
+
+def test_train_cnn_wo_fourier():
+    from gaussian_sources.train_cnn import main
+
+    data_path = "./tests/build/gaussian_sources/wo_fourier"
+    path_model = "./tests/build/gaussian_sources/wo_fourier/test.model"
+    arch = "UNet_denoise"
+    norm_path = "./tests/build/gaussian_sources/wo_fourier/normalization_factors.csv"
+    epochs = "5"
+    lr = "1e-3"
+    lr_type = "mse"
+    bs = "2"
+
+    runner = CliRunner()
+    options = [
+        data_path,
+        path_model,
+        arch,
+        norm_path,
+        epochs,
+        lr,
+        lr_type,
+        bs,
+        "-fourier",
+        False,
+        "-amp_phase",
+        None,
+        "-pretrained",
+        False,
+        "-inspection",
+        False,
+        "-test",
+        True,
     ]
     result = runner.invoke(main, options)
     print(traceback.print_exception(*result.exc_info))
