842a41b30469b87de2ffbac3731b2f1515a86ea9
Felix Geyer <felix.geyer@tu-dortmund.de>
2020-04-22 18:51:41 +0200

    Default test to False and added lr_find

--- gaussian_sources/find_lr.py
+++ gaussian_sources/find_lr.py
@@ -44,7 +44,7 @@ from dl_framework.inspection import plot_lr_loss
 )
 @click.option("-save", type=bool, required=False, help="save the lr vs loss plot")
 @click.option(
-    "-test", type=bool, default=True, required=True, help="Disable logger in tests"
+    "-test", type=bool, default=False, required=True, help="Disable logger in tests"
 )
 def main(
     data_path,
@@ -60,7 +60,7 @@ def main(
     pretrained=False,
     pretrained_model=None,
     save=False,
-    test=True,
+    test=False,
 ):
     """
     Train the neural network with existing training and validation data.
@@ -101,6 +101,7 @@ def main(
         loss_func,
         test=test,
         cbfs=cbfs,
+        lr_find=True,
     )
 
     # use pre-trained model if asked
