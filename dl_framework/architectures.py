0b9e33c5c33326ef7e352a22364718a9379e199b
Felix Geyer <felix.geyer@tu-dortmund.de>
2020-04-27 15:24:04 +0200

    Changed filter deep for new architecture for amp and phase

--- dl_framework/architectures.py
+++ dl_framework/architectures.py
@@ -14,6 +14,8 @@ from dl_framework.model import (
     symmetry,
     shape,
     GeneralELU,
+    conv_phase,
+    conv_amp,
 )
 from functools import partial
 
@@ -524,190 +526,151 @@ class filter(nn.Module):
 class filter_deep(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=2,
-                out_channels=4,
-                kernel_size=(23, 23),
-                stride=1,
-                padding=11,
-                dilation=1,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(4),
+        self.conv1_amp = nn.Sequential(
+            *conv_amp(1, 4, (23, 23), 1, 11, 1)
+        )
+        self.conv1_phase = nn.Sequential(
+            *conv_phase(1, 4, (23, 23), 1, 11, 1, add=-2.1415)
+        )
+        self.conv2_amp = nn.Sequential(
+             *conv_amp(4, 8, (21, 21), 1, 10, 1)
+        )
+        self.conv2_phase = nn.Sequential(
+             *conv_phase(4, 8, (21, 21), 1, 10, 1, add=-2.1415)
+        )
+        self.conv3_amp = nn.Sequential(
+             *conv_amp(8, 12, (17, 17), 1, 8, 1)
+        )
+        self.conv3_phase = nn.Sequential(
+             *conv_phase(8, 12, (17, 17), 1, 8, 1, add=-2.1415)
+        )
+        self.conv_con1_amp = nn.Sequential(
+            LocallyConnected2d(12, 1, 63, 1, stride=1, bias=False),
+            nn.BatchNorm2d(1),
+            nn.ReLU(),
+        )
+        self.conv_con1_phase = nn.Sequential(
+            LocallyConnected2d(12, 1, 63, 1, stride=1, bias=False),
+            nn.BatchNorm2d(1),
             GeneralELU(-2.1415),
         )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=4,
-                out_channels=8,
-                kernel_size=(21, 21),
-                stride=1,
-                padding=10,
-                dilation=1,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(8),
+
+        self.conv4_amp = nn.Sequential(
+             *conv_amp(1, 4, (5, 5), 1, 3, 2)
+        )
+        self.conv4_phase = nn.Sequential(
+             *conv_phase(1, 4, (5, 5), 1, 3, 2, add=-2.1415)
+        )
+        self.conv5_amp = nn.Sequential(
+             *conv_amp(4, 8, (5, 5), 1, 2, 1)
+        )
+        self.conv5_phase = nn.Sequential(
+             *conv_phase(4, 8, (5, 5), 1, 2, 1, add=-2.1415)
+        )
+        self.conv6_amp = nn.Sequential(
+             *conv_amp(8, 12, (3, 3), 1, 3, 2)
+        )
+        self.conv6_phase = nn.Sequential(
+             *conv_phase(8, 12, (3, 3), 1, 3, 2, add=-2.1415)
+        )
+        self.conv7_amp = nn.Sequential(
+             *conv_amp(12, 16, (3, 3), 1, 1, 1)
+        )
+        self.conv7_phase = nn.Sequential(
+             *conv_phase(12, 16, (3, 3), 1, 1, 1, add=-2.1415)
+        )
+        self.conv_con2_amp = nn.Sequential(
+            LocallyConnected2d(16, 1, 63, 1, stride=1, bias=False),
+            nn.BatchNorm2d(1),
+            nn.ReLU(),
+        )
+        self.conv_con2_phase = nn.Sequential(
+            LocallyConnected2d(16, 1, 63, 1, stride=1, bias=False),
+            nn.BatchNorm2d(1),
             GeneralELU(-2.1415),
         )
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=8,
-                out_channels=12,
-                kernel_size=(17, 17),
-                stride=1,
-                padding=8,
-                dilation=1,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(12),
-            GeneralELU(-2.1415),
+
+        self.conv8_amp = nn.Sequential(
+             *conv_amp(1, 4, (3, 3), 1, 1, 1)
         )
-        self.conv_con1 = nn.Sequential(
-            LocallyConnected2d(12, 2, 63, 1, stride=1, bias=False),
-            nn.BatchNorm2d(2),
-            GeneralELU(-2.1415),
+        self.conv8_phase = nn.Sequential(
+             *conv_phase(1, 4, (3, 3), 1, 1, 1, add=-2.1415)
         )
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=2,
-                out_channels=4,
-                kernel_size=(5, 5),
-                stride=1,
-                padding=3,
-                dilation=2,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(4),
-            GeneralELU(-2.1415),
+        self.conv9_amp = nn.Sequential(
+             *conv_amp(4, 8, (3, 3), 1, 1, 1)
         )
-        self.conv5 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=4,
-                out_channels=8,
-                kernel_size=(5, 5),
-                stride=1,
-                padding=2,
-                dilation=1,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(8),
-            GeneralELU(-2.1415),
+        self.conv9_phase = nn.Sequential(
+             *conv_phase(4, 8, (3, 3), 1, 1, 1, add=-2.1415)
         )
-        self.conv6 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=8,
-                out_channels=12,
-                kernel_size=(3, 3),
-                stride=1,
-                padding=3,
-                dilation=2,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(12),
-            GeneralELU(-2.1415),
+        self.conv10_amp = nn.Sequential(
+             *conv_amp(8, 12, (3, 3), 1, 2, 2)
         )
-        self.conv7 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=12,
-                out_channels=20,
-                kernel_size=(3, 3),
-                stride=1,
-                padding=1,
-                dilation=1,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(20),
-            GeneralELU(-2.1415),
+        self.conv10_phase = nn.Sequential(
+             *conv_phase(8, 12, (3, 3), 1, 2, 2, add=-2.1415)
         )
-        self.conv_con2 = nn.Sequential(
-            LocallyConnected2d(20, 2, 63, 1, stride=1, bias=False),
-            nn.BatchNorm2d(2),
-            GeneralELU(-2.1415),
+        self.conv_con3_amp = nn.Sequential(
+            LocallyConnected2d(12, 1, 63, 1, stride=1, bias=False),
+            nn.BatchNorm2d(1),
+            nn.ReLU(),
         )
-        self.conv8 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=2,
-                out_channels=4,
-                kernel_size=(3, 3),
-                stride=1,
-                padding=1,
-                dilation=1,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(4),
-            GeneralELU(-2.1415),
-        )
-        self.conv9 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=4,
-                out_channels=8,
-                kernel_size=(3, 3),
-                stride=1,
-                padding=1,
-                dilation=1,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(8),
-            GeneralELU(-2.1415),
-        )
-        self.conv10 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=8,
-                out_channels=12,
-                kernel_size=(3, 3),
-                stride=1,
-                padding=2,
-                dilation=2,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(12),
-            GeneralELU(-2.1415),
-        )
-        self.conv_con3 = nn.Sequential(
-            LocallyConnected2d(12, 2, 63, 1, stride=1, bias=False),
-            nn.BatchNorm2d(2),
+        self.conv_con3_phase = nn.Sequential(
+            LocallyConnected2d(12, 1, 63, 1, stride=1, bias=False),
+            nn.BatchNorm2d(1),
             GeneralELU(-2.1415),
         )
         self.symmetry_real = Lambda(symmetry)
         self.symmetry_imag = Lambda(partial(symmetry, mode='imag'))
-        self.elu = nn.ELU()
 
     def forward(self, x):
         inp = x.clone()
+        amp = x[:, 0, :].unsqueeze(1)
+        phase = x[:, 1, :].unsqueeze(1)
 
         # First block
-        x = self.conv1(x)
-        x = self.conv2(x)
-        conv3 = self.conv3(x)
-        x = self.conv_con1(conv3)
+        amp = self.conv1_amp(amp)
+        phase = self.conv1_phase(phase)
+
+        amp = self.conv2_amp(amp)
+        phase = self.conv2_phase(phase)
+
+        amp = self.conv3_amp(amp)
+        phase = self.conv3_phase(phase)
+
+        amp = self.conv_con1_amp(amp)
+        phase = self.conv_con1_phase(phase)
 
         # Second block
-        x = self.conv4(x)
-        x = self.conv5(x)
-        x = self.conv6(x)
-        conv7 = self.conv7(x)
-        x = self.conv_con2(conv7)
+        amp = self.conv4_amp(amp)
+        phase = self.conv4_phase(phase)
+
+        amp = self.conv5_amp(amp)
+        phase = self.conv5_phase(phase)
+
+        amp = self.conv6_amp(amp)
+        phase = self.conv6_phase(phase)
+
+        amp = self.conv7_amp(amp)
+        phase = self.conv7_phase(phase)
+
+        amp = self.conv_con2_amp(amp)
+        phase = self.conv_con2_phase(phase)
 
         # Third block
-        x = self.conv8(x)
-        x = self.conv9(x)
-        conv10 = self.conv10(x)
-        x = self.conv_con3(conv10)
+        amp = self.conv8_amp(amp)
+        phase = self.conv8_phase(phase)
 
-        x = x.clone()
-        x = x + inp
-        x0 = self.symmetry_real(x[:, 0]).reshape(-1, 1, 63, 63)
-        x1 = self.symmetry_imag(x[:, 1]).reshape(-1, 1, 63, 63)
+        amp = self.conv9_amp(amp)
+        phase = self.conv9_phase(phase)
+
+        amp = self.conv10_amp(amp)
+        phase = self.conv10_phase(phase)
+
+        amp = self.conv_con3_amp(amp)
+        phase = self.conv_con3_phase(phase)
+
+        amp = amp + inp[:, 0].unsqueeze(1)
+        phase = phase + inp[:, 1].unsqueeze(1)
+        x0 = self.symmetry_real(amp).reshape(-1, 1, 63, 63)
+        x1 = self.symmetry_imag(phase).reshape(-1, 1, 63, 63)
         comb = torch.cat([x0, x1], dim=1)
         return comb
