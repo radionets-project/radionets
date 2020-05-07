475f6f024c130b72f74f173bee9d551715739d9d
Felix Geyer <felix.geyer@tu-dortmund.de>
2020-05-07 11:31:19 +0200

    Changed loading of test_ds and shapes

--- tests/test_visualization/test_eval_plots.py
+++ tests/test_visualization/test_eval_plots.py
@@ -1,34 +1,32 @@
 import os
 from pathlib import Path
 
-from tests.test_visualization.test_save_load_preds import test_create_h5_dataset
 
-test_ds = test_create_h5_dataset()
+def test_load_data():
+    from dl_framework.data import load_data
+
+    data_path = "./tests/build/gaussian_sources/wo_fourier"
+    test_ds = load_data(data_path, "test", False)
+    return test_ds
 
 
 def test_reshape_split():
     from gaussian_sources.inspection import reshape_split
 
+    test_ds = test_load_data()
+
     img_without = test_ds[0][1]
     img_with = test_ds[0][0].reshape(-1)
 
-    assert reshape_split(img_without).shape == (64, 64)
-    assert reshape_split(img_with)[0].shape == (64, 64)
-    assert reshape_split(img_with)[1].shape == (64, 64)
-
-
-def test_open_csv():
-    from gaussian_sources.inspection import open_csv
-
-    path = "tests/test_data/without_fourier/"
-    mode = "predictions"
-
-    assert open_csv(path, mode)[0].shape == (20, 4096)
-    assert open_csv(path, mode)[1].shape == (20,)
+    assert reshape_split(img_without).shape == (63, 63)
+    assert reshape_split(img_with)[0].shape == (63, 63)
+    assert reshape_split(img_with)[1].shape == (63, 63)
 
 
 def test_visualize_without_fourier():
     from gaussian_sources.inspection import visualize_without_fourier
+
+    test_ds = test_load_data()
 
     build = "tests/build/"
     build_diff = "tests/build/diff"
@@ -49,6 +47,8 @@ def test_visualize_without_fourier():
 def test_visualize_with_fourier():
     from gaussian_sources.inspection import visualize_with_fourier
 
+    test_ds = test_load_data()
+
     img_input, img_pred, img_truth = (
         test_ds[0][0].reshape(-1),
         test_ds[1][0].reshape(-1),
@@ -63,14 +63,16 @@ def test_visualize_with_fourier():
         i, img_input, img_pred, img_truth, False, build
     )
 
-    assert real_pred.shape == (64, 64)
-    assert imag_pred.shape == (64, 64)
-    assert real_truth.shape == (64, 64)
-    assert imag_truth.shape == (64, 64)
+    assert real_pred.shape == (63, 63)
+    assert imag_pred.shape == (63, 63)
+    assert real_truth.shape == (63, 63)
+    assert imag_truth.shape == (63, 63)
 
 
 def test_compute_fft():
     from gaussian_sources.inspection import visualize_fft, reshape_split
+
+    test_ds = test_load_data()
 
     img_pred, img_truth = (
         test_ds[0][0].numpy().reshape(-1),
@@ -87,14 +89,16 @@ def test_compute_fft():
         i, real_pred, imag_pred, real_truth, imag_truth, False, build
     )
 
-    assert ifft_pred.shape == (64, 64)
-    assert ifft_truth.shape == (64, 64)
+    assert ifft_pred.shape == (63, 63)
+    assert ifft_truth.shape == (63, 63)
 
     return ifft_pred, ifft_truth
 
 
 def test_hist_difference():
     from gaussian_sources.inspection import hist_difference
+
+    test_ds = test_load_data()
 
     img_pred, img_truth = (
         test_ds[0][1].numpy(),
@@ -108,6 +112,8 @@ def test_hist_difference():
 def test_plot_difference():
     from gaussian_sources.inspection import plot_difference
 
+    test_ds = test_load_data()
+
     build = "tests/build/"
     ifft_pred, ifft_truth = test_compute_fft()
     img_pred, img_truth = (
@@ -116,7 +122,7 @@ def test_plot_difference():
     )
     dr_fourier = plot_difference(0, ifft_pred, ifft_truth, build)
     dr_wo_fourier = plot_difference(
-        0, img_pred.reshape(64, 64), img_truth.reshape(64, 64), build
+        0, img_pred.reshape(63, 63), img_truth.reshape(63, 63), build
     )
 
     assert dr_fourier.dtype == float
@@ -126,9 +132,11 @@ def test_plot_difference():
 def test_blob_detection():
     from gaussian_sources.inspection import blob_detection
 
+    test_ds = test_load_data()
+
     img_pred, img_truth = (
-        test_ds[0][1].numpy().reshape(64, 64),
-        test_ds[9][1].numpy().reshape(64, 64),
+        test_ds[0][1].numpy().reshape(63, 63),
+        test_ds[3][1].numpy().reshape(63, 63),
     )
     build = "tests/build/"
 
