05fb7be97fa893abc5bcc5a8a4d2722c64632f7a
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-05-12 18:36:04 +0200

    refactoring filter architectures

--- dl_framework/architectures.py
+++ dl_framework/architectures.py
@@ -913,220 +913,75 @@ class filter_deep_phase(nn.Module):
         return x0
 
 
+from dl_framework.uncertainty_arch import (
+    block_1_a,
+    block_2_a,
+    block_3_a,
+    block_1_a_unc,
+    block_2_a_unc,
+    block_3_a_unc,
+)
+
+
 class filter_deep_amp_unc(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=1,
-                out_channels=4,
-                kernel_size=(23, 23),
-                stride=1,
-                padding=11,
-                dilation=1,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(4),
-            nn.ELU(),
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=4,
-                out_channels=8,
-                kernel_size=(21, 21),
-                stride=1,
-                padding=10,
-                dilation=1,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(8),
-            nn.ELU(),
-        )
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=8,
-                out_channels=12,
-                kernel_size=(17, 17),
-                stride=1,
-                padding=8,
-                dilation=1,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(12),
-            nn.ELU(),
-        )
-        self.conv_con1 = nn.Sequential(
-            LocallyConnected2d(12, 2, 63, 1, stride=1, bias=False),
-            nn.BatchNorm2d(2),
-            nn.ELU(),
-        )
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=2,
-                out_channels=4,
-                kernel_size=(5, 5),
-                stride=1,
-                padding=3,
-                dilation=2,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(4),
-            nn.ELU(),
-        )
-        self.conv5 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=4,
-                out_channels=8,
-                kernel_size=(5, 5),
-                stride=1,
-                padding=2,
-                dilation=1,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(8),
-            nn.ELU(),
-        )
-        self.conv6 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=8,
-                out_channels=12,
-                kernel_size=(3, 3),
-                stride=1,
-                padding=3,
-                dilation=2,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(12),
-            nn.ELU(),
-        )
-        self.conv7 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=12,
-                out_channels=20,
-                kernel_size=(3, 3),
-                stride=1,
-                padding=1,
-                dilation=1,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(20),
-            nn.ELU(),
-        )
-        self.conv_con2 = nn.Sequential(
-            LocallyConnected2d(20, 2, 63, 1, stride=1, bias=False),
-            nn.BatchNorm2d(2),
-            nn.ELU(),
-        )
-        self.conv8 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=2,
-                out_channels=4,
-                kernel_size=(3, 3),
-                stride=1,
-                padding=1,
-                dilation=1,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(4),
-            nn.ELU(),
-        )
-        self.conv9 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=4,
-                out_channels=8,
-                kernel_size=(3, 3),
-                stride=1,
-                padding=1,
-                dilation=1,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(8),
-            nn.ELU(),
-        )
-        self.conv10 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=8,
-                out_channels=12,
-                kernel_size=(3, 3),
-                stride=1,
-                padding=2,
-                dilation=2,
-                padding_mode="zeros",
-                bias=False,
-            ),
-            nn.BatchNorm2d(12),
-            nn.ELU(),
-        )
-        self.conv_con3 = nn.Sequential(
-            LocallyConnected2d(12, 2, 63, 1, stride=1, bias=False),
-            nn.BatchNorm2d(2),
-            nn.ELU(),
-        )
+        self.block1 = block_1_a()
+        self.block2 = block_2_a()
+        self.block3 = block_3_a()
+        self.block1_unc = block_1_a_unc()
+        self.block2_unc = block_2_a_unc()
+        self.block3_unc = block_3_a_unc()
         self.symmetry = Lambda(symmetry)
-        self.elu = nn.ELU()
 
     def forward(self, x):
         x = x[:, 0].unsqueeze(1)
         inp = x.clone()
 
-        # First block
-        x = self.conv1(x)
-        x = self.conv2(x)
-        conv3 = self.conv3(x)
-        x = self.conv_con1(conv3)
+        # Blocks to predict amp
+        x0 = self.block1(x)
+        x0 = self.block2(x0)
+        x0 = self.block3(x0)
 
-        # Second block
-        x = self.conv4(x)
-        x = self.conv5(x)
-        x = self.conv6(x)
-        conv7 = self.conv7(x)
-        x = self.conv_con2(conv7)
-
-        # Third block
-        x = self.conv8(x)
-        x = self.conv9(x)
-        conv10 = self.conv10(x)
-        x = self.conv_con3(conv10)
-
-        x = x.clone()
-        x = x + inp
+        x0 = x0.clone()
+        x0 = x0 + inp
         x0 = self.symmetry(x[:, 0]).reshape(-1, 1, 63, 63)
-        x[:, 1][inp[:, 0] == 0] += 1e-5 + 1
-        x0[:, 0][inp[:, 0] != 0] = inp[:, 0][inp[:, 0] != 0]
-        x[:, 1][inp[:, 0] != 0] = 1e-8
-        x1 = self.symmetry(x[:, 1]).reshape(-1, 1, 63, 63)
+        x0[inp != 0] = inp[inp != 0]
+
+        # Blocks to predict uncertainty
+        x1 = self.block1_unc(x)
+        x1 = self.block2_unc(x1)
+        x1 = self.block3_unc(x1)
+
+        x1 = x1.clone()
+        x1 = x1 + inp
+        x1[inp == 0] += 1+1e-5
+        x1 = self.symmetry(x1[:, 0]).reshape(-1, 1, 63, 63)
+        x1[inp != 0] = 1e-8
+
         out = torch.cat([x0, x1, inp], dim=1)
         return out
 
 
 from dl_framework.uncertainty_arch import (
-    block_1,
-    block_2,
-    block_3,
-    block_1_unc,
-    block_2_unc,
-    block_3_unc,
+    block_1_p,
+    block_2_p,
+    block_3_p,
+    block_1_p_unc,
+    block_2_p_unc,
+    block_3_p_unc,
 )
 
 
 class filter_deep_phase_unc(nn.Module):
     def __init__(self):
         super().__init__()
-        self.block1 = block_1()
-        self.block2 = block_2()
-        self.block3 = block_3()
-        self.block1_unc = block_1_unc()
-        self.block2_unc = block_2_unc()
-        self.block3_unc = block_3_unc()
+        self.block1 = block_1_p()
+        self.block2 = block_2_p()
+        self.block3 = block_3_p()
+        self.block1_unc = block_1_p_unc()
+        self.block2_unc = block_2_p_unc()
+        self.block3_unc = block_3_p_unc()
         self.symmetry = Lambda(partial(symmetry, mode="imag"))
         self.symmetry_unc = Lambda(symmetry)
         self.elu_phase = GeneralELU(add=-(pi - 1), maxv=pi)
@@ -1147,7 +1002,7 @@ class filter_deep_phase_unc(nn.Module):
         # x0 = self.phase_range(x0)
         x0[inp != 0] = inp[inp != 0]
         x0 = self.symmetry(x0[:, 0]).reshape(-1, 1, 63, 63)
-        x0 = self.elu_phase(x0)
+        # x0 = self.elu_phase(x0)
 
         # Blocks to predict uncertainty
         x1 = self.block1_unc(x)
--- dl_framework/uncertainty_arch.py
+++ dl_framework/uncertainty_arch.py
@@ -3,191 +3,66 @@ from dl_framework.model import GeneralELU, LocallyConnected2d
 from math import pi
 
 
-def block_1():
+def conv(ni, nc, ks, stride, padding, dilation, act):
+    conv = (
+        nn.Conv2d(
+            in_channels=ni,
+            out_channels=nc,
+            kernel_size=(ks, ks),
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            bias=False,
+            padding_mode="replicate",
+        ),
+    )
+    bn = (nn.BatchNorm2d(nc),)
+    layers = [*conv, *bn, act]
+    return layers
+
+
+def block_1_a():
     layers = [
-        nn.Conv2d(
-            in_channels=1,
-            out_channels=4,
-            kernel_size=(23, 23),
-            stride=1,
-            padding=11,
-            dilation=1,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(4),
-        GeneralELU(add=-(pi-1), maxv=pi),
-        nn.Conv2d(
-            in_channels=4,
-            out_channels=8,
-            kernel_size=(21, 21),
-            stride=1,
-            padding=10,
-            dilation=1,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(8),
-        GeneralELU(add=-(pi-1), maxv=pi),
-        nn.Conv2d(
-            in_channels=8,
-            out_channels=12,
-            kernel_size=(17, 17),
-            stride=1,
-            padding=8,
-            dilation=1,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(12),
-        GeneralELU(add=-(pi-1), maxv=pi),
+        *conv(1, 4, 23, 1, 11, 1, GeneralELU(add=+1)),
+        *conv(4, 8, 21, 1, 10, 1, GeneralELU(add=+1)),
+        *conv(8, 12, 17, 1, 8, 1, GeneralELU(add=+1)),
         LocallyConnected2d(12, 1, 63, 1, stride=1, bias=False),
         nn.BatchNorm2d(1),
-        GeneralELU(add=-(pi-1), maxv=pi),
+        GeneralELU(add=+1),
     ]
     return nn.Sequential(*layers)
 
 
-def block_2():
+def block_2_a():
     layers = [
-        nn.Conv2d(
-            in_channels=1,
-            out_channels=4,
-            kernel_size=(5, 5),
-            stride=1,
-            padding=2,
-            dilation=1,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(4),
-        GeneralELU(add=-(pi-1), maxv=pi),
-        nn.Conv2d(
-            in_channels=4,
-            out_channels=8,
-            kernel_size=(5, 5),
-            stride=1,
-            padding=3,
-            dilation=2,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(8),
-        GeneralELU(add=-(pi-1), maxv=pi),
-        nn.Conv2d(
-            in_channels=8,
-            out_channels=12,
-            kernel_size=(3, 3),
-            stride=1,
-            padding=1,
-            dilation=1,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(12),
-        GeneralELU(add=-(pi-1), maxv=pi),
-        nn.Conv2d(
-            in_channels=12,
-            out_channels=20,
-            kernel_size=(3, 3),
-            stride=1,
-            padding=3,
-            dilation=2,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(20),
-        GeneralELU(add=-(pi-1), maxv=pi),
-        LocallyConnected2d(20, 1, 63, 1, stride=1, bias=False),
+        *conv(1, 4, 5, 1, 2, 1, GeneralELU(add=+1)),
+        *conv(4, 8, 5, 1, 4, 2, GeneralELU(add=+1)),
+        *conv(8, 12, 3, 1, 1, 1, GeneralELU(add=+1)),
+        *conv(12, 16, 3, 1, 2, 2, GeneralELU(add=+1)),
+        LocallyConnected2d(16, 1, 63, 1, stride=1, bias=False),
         nn.BatchNorm2d(1),
-        GeneralELU(add=-(pi-1), maxv=pi),
+        GeneralELU(add=+1),
     ]
     return nn.Sequential(*layers)
 
 
-def block_3():
+def block_3_a():
     layers = [
-        nn.Conv2d(
-            in_channels=1,
-            out_channels=4,
-            kernel_size=(3, 3),
-            stride=1,
-            padding=1,
-            dilation=1,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(4),
-        GeneralELU(add=-(pi-1), maxv=pi),
-        nn.Conv2d(
-            in_channels=4,
-            out_channels=8,
-            kernel_size=(3, 3),
-            stride=1,
-            padding=1,
-            dilation=1,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(8),
-        GeneralELU(add=-(pi-1), maxv=pi),
-        nn.Conv2d(
-            in_channels=8,
-            out_channels=12,
-            kernel_size=(3, 3),
-            stride=1,
-            padding=2,
-            dilation=2,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(12),
-        GeneralELU(add=-(pi-1), maxv=pi),
+        *conv(1, 4, 3, 1, 1, 1, GeneralELU(add=+1)),
+        *conv(4, 8, 3, 1, 1, 1, GeneralELU(add=+1)),
+        *conv(8, 12, 3, 1, 2, 2, GeneralELU(add=+1)),
         LocallyConnected2d(12, 1, 63, 1, stride=1, bias=False),
         nn.BatchNorm2d(1),
-        GeneralELU(add=-(pi-1), maxv=pi),
+        GeneralELU(add=+1),
     ]
     return nn.Sequential(*layers)
 
 
-def block_1_unc():
+def block_1_a_unc():
     layers = [
-        nn.Conv2d(
-            in_channels=1,
-            out_channels=4,
-            kernel_size=(23, 23),
-            stride=1,
-            padding=11,
-            dilation=1,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(4),
-        GeneralELU(add=+1+1e-5),
-        nn.Conv2d(
-            in_channels=4,
-            out_channels=8,
-            kernel_size=(21, 21),
-            stride=1,
-            padding=10,
-            dilation=1,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(8),
-        GeneralELU(add=+1+1e-5),
-        nn.Conv2d(
-            in_channels=8,
-            out_channels=12,
-            kernel_size=(17, 17),
-            stride=1,
-            padding=8,
-            dilation=1,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(12),
-        GeneralELU(add=+1+1e-5),
+        *conv(1, 4, 23, 1, 11, 1, GeneralELU(add=+1+1e-5)),
+        *conv(4, 8, 21, 1, 10, 1, GeneralELU(add=+1+1e-5)),
+        *conv(8, 12, 17, 1, 8, 1, GeneralELU(add=+1+1e-5)),
         LocallyConnected2d(12, 1, 63, 1, stride=1, bias=False),
         nn.BatchNorm2d(1),
         GeneralELU(add=+1+1e-5),
@@ -195,103 +70,100 @@ def block_1_unc():
     return nn.Sequential(*layers)
 
 
-def block_2_unc():
+def block_2_a_unc():
     layers = [
-        nn.Conv2d(
-            in_channels=1,
-            out_channels=4,
-            kernel_size=(5, 5),
-            stride=1,
-            padding=2,
-            dilation=1,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(4),
-        GeneralELU(add=+1+1e-5),
-        nn.Conv2d(
-            in_channels=4,
-            out_channels=8,
-            kernel_size=(5, 5),
-            stride=1,
-            padding=3,
-            dilation=2,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(8),
-        GeneralELU(add=-(pi-1)),
-        nn.Conv2d(
-            in_channels=8,
-            out_channels=12,
-            kernel_size=(3, 3),
-            stride=1,
-            padding=1,
-            dilation=1,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(12),
-        GeneralELU(add=+1+1e-5),
-        nn.Conv2d(
-            in_channels=12,
-            out_channels=20,
-            kernel_size=(3, 3),
-            stride=1,
-            padding=3,
-            dilation=2,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(20),
-        GeneralELU(add=+1+1e-5),
-        LocallyConnected2d(20, 1, 63, 1, stride=1, bias=False),
+        *conv(1, 4, 5, 1, 2, 1, GeneralELU(add=+1+1e-5)),
+        *conv(4, 8, 5, 1, 4, 2, GeneralELU(add=+1+1e-5)),
+        *conv(8, 12, 3, 1, 1, 1, GeneralELU(add=+1+1e-5)),
+        *conv(12, 16, 3, 1, 2, 2, GeneralELU(add=+1+1e-5)),
+        LocallyConnected2d(16, 1, 63, 1, stride=1, bias=False),
         nn.BatchNorm2d(1),
         GeneralELU(add=+1+1e-5),
     ]
     return nn.Sequential(*layers)
 
 
-def block_3_unc():
+def block_3_a_unc():
     layers = [
-        nn.Conv2d(
-            in_channels=1,
-            out_channels=4,
-            kernel_size=(3, 3),
-            stride=1,
-            padding=1,
-            dilation=1,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(4),
-        GeneralELU(add=+1+1e-5),
-        nn.Conv2d(
-            in_channels=4,
-            out_channels=8,
-            kernel_size=(3, 3),
-            stride=1,
-            padding=1,
-            dilation=1,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(8),
-        GeneralELU(add=+1+1e-5),
-        nn.Conv2d(
-            in_channels=8,
-            out_channels=12,
-            kernel_size=(3, 3),
-            stride=1,
-            padding=2,
-            dilation=2,
-            padding_mode="zeros",
-            bias=False,
-        ),
-        nn.BatchNorm2d(12),
-        GeneralELU(add=+1+1e-5),
+        *conv(1, 4, 3, 1, 1, 1, GeneralELU(add=+1+1e-5)),
+        *conv(4, 8, 3, 1, 1, 1, GeneralELU(add=+1+1e-5)),
+        *conv(8, 12, 3, 1, 2, 2, GeneralELU(add=+1+1e-5)),
         LocallyConnected2d(12, 1, 63, 1, stride=1, bias=False),
         nn.BatchNorm2d(1),
         GeneralELU(add=+1+1e-5),
+    ]
+    return nn.Sequential(*layers)
+
+
+def block_1_p():
+    layers = [
+        *conv(1, 4, 23, 1, 11, 1, GeneralELU(add=-(pi - 1))),
+        *conv(4, 8, 21, 1, 10, 1, GeneralELU(add=-(pi - 1))),
+        *conv(8, 12, 17, 1, 8, 1, GeneralELU(add=-(pi - 1))),
+        LocallyConnected2d(12, 1, 63, 1, stride=1, bias=False),
+        nn.BatchNorm2d(1),
+        GeneralELU(add=-(pi - 1)),
+    ]
+    return nn.Sequential(*layers)
+
+
+def block_2_p():
+    layers = [
+        *conv(1, 8, 5, 1, 2, 1, GeneralELU(add=-(pi - 1))),
+        *conv(8, 16, 5, 1, 4, 2, GeneralELU(add=-(pi - 1))),
+        *conv(16, 24, 3, 1, 1, 1, GeneralELU(add=-(pi - 1))),
+        *conv(24, 32, 3, 1, 2, 2, GeneralELU(add=-(pi - 1))),
+        LocallyConnected2d(32, 1, 63, 1, stride=1, bias=False),
+        nn.BatchNorm2d(1),
+        GeneralELU(add=-(pi - 1)),
+    ]
+    return nn.Sequential(*layers)
+
+
+def block_3_p():
+    layers = [
+        *conv(1, 8, 3, 1, 1, 1, GeneralELU(add=-(pi - 1))),
+        *conv(8, 16, 3, 1, 1, 1, GeneralELU(add=-(pi - 1))),
+        *conv(16, 24, 3, 1, 2, 2, GeneralELU(add=-(pi - 1))),
+        LocallyConnected2d(24, 1, 63, 1, stride=1, bias=False),
+        nn.BatchNorm2d(1),
+        GeneralELU(add=-(pi - 1)),
+    ]
+    return nn.Sequential(*layers)
+
+
+def block_1_p_unc():
+    layers = [
+        *conv(1, 4, 23, 1, 11, 1, GeneralELU(add=+(1 + 1e-5))),
+        *conv(4, 8, 21, 1, 10, 1, GeneralELU(add=+(1 + 1e-5))),
+        *conv(8, 12, 17, 1, 8, 1, GeneralELU(add=+(1 + 1e-5))),
+        LocallyConnected2d(12, 1, 63, 1, stride=1, bias=False),
+        nn.BatchNorm2d(1),
+        GeneralELU(add=+1 + 1e-5),
+    ]
+    return nn.Sequential(*layers)
+
+
+def block_2_p_unc():
+    layers = [
+        *conv(1, 4, 5, 1, 2, 1, GeneralELU(add=+(1 + 1e-5))),
+        *conv(4, 8, 5, 1, 4, 2, GeneralELU(add=+(1 + 1e-5))),
+        *conv(8, 12, 3, 1, 1, 1, GeneralELU(add=+(1 + 1e-5))),
+        *conv(12, 16, 3, 1, 2, 2, GeneralELU(add=+(1 + 1e-5))),
+        LocallyConnected2d(16, 1, 63, 1, stride=1, bias=False),
+        nn.BatchNorm2d(1),
+        GeneralELU(add=+1 + 1e-5),
+    ]
+    return nn.Sequential(*layers)
+
+
+def block_3_p_unc():
+    layers = [
+        *conv(1, 4, 3, 1, 1, 1, GeneralELU(add=+(1 + 1e-5))),
+        *conv(4, 8, 3, 1, 1, 1, GeneralELU(add=+(1 + 1e-5))),
+        *conv(8, 12, 3, 1, 2, 2, GeneralELU(add=+(1 + 1e-5))),
+        LocallyConnected2d(12, 1, 63, 1, stride=1, bias=False),
+        nn.BatchNorm2d(1),
+        GeneralELU(add=+1 + 1e-5),
     ]
     return nn.Sequential(*layers)
