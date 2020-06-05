a8247529a4d93c0bafd86097465989adb1ea5929
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-06-05 11:05:55 +0200

    more visualization plots

--- simulations/visualize_sampling.py
+++ simulations/visualize_sampling.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 from simulations.gaussian_simulations import gaussian_source
@@ -28,6 +29,36 @@ plt.savefig(
 plot_source(sim_source, ft=True, log=True)
 plt.savefig(
     "examples/fft_gaussian_source.pdf", dpi=100, bbox_inches="tight", pad_inches=0.01,
+)
+
+fft = FT(sim_source)
+
+plot_source(fft.real, ft=False, log=False)
+plt.xlabel("u")
+plt.ylabel("v")
+plt.savefig(
+    "examples/fft_gaussian_source_real.pdf", dpi=100, bbox_inches="tight", pad_inches=0.01,
+)
+
+plot_source(fft.imag, ft=False, log=False)
+plt.xlabel("u")
+plt.ylabel("v")
+plt.savefig(
+    "examples/fft_gaussian_source_imag.pdf", dpi=100, bbox_inches="tight", pad_inches=0.01,
+)
+
+plot_source(np.abs(fft), ft=False, log=True)
+plt.xlabel("u")
+plt.ylabel("v")
+plt.savefig(
+    "examples/fft_gaussian_source_amp.pdf", dpi=100, bbox_inches="tight", pad_inches=0.01,
+)
+
+plot_source(np.angle(fft), ft=False, log=False)
+plt.xlabel("u")
+plt.ylabel("v")
+plt.savefig(
+    "examples/fft_gaussian_source_phase.pdf", dpi=100, bbox_inches="tight", pad_inches=0.01,
 )
 
 ant = antenna(*get_antenna_config("./layouts/vlba.txt"))
