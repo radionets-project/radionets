0c18e765ba7f037e43ea4c0b13dcb11f9bc925af
Felix Geyer <felix.geyer@tu-dortmund.de>
2020-06-16 12:36:26 +0200

    Added deleting of each gaussian_sources file after sampling

--- gaussian_sources/create_fft_pairs.py
+++ gaussian_sources/create_fft_pairs.py
@@ -1,4 +1,5 @@
 import click
+import os
 import numpy as np
 from tqdm import tqdm
 from dl_framework.data import (
@@ -109,10 +110,11 @@ def main(
                 # with open(out, 'wb') as f:
                 #     np.save(f, bundle_samp)
                 #     np.save(f, bundle_fft)
-                # savez_compressed(out, x=bundle_samp, y=bundle_fft)
-                save_fft_pair(out, bundle_samp, bundle_fft)
+                savez_compressed(out, x=bundle_samp, y=bundle_fft)
+                # save_fft_pair(out, bundle_samp, bundle_fft)
             else:
                 save_fft_pair(out, bundle_samp, images)
+            os.remove(path)
 
 
 if __name__ == "__main__":
