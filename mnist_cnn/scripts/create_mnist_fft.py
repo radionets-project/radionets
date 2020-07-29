bf40af0ac1f3d08343eb63af7c38e126f937c7b4
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-07-29 11:25:42 +0200

    fixing dataset creation

--- mnist_cnn/scripts/create_mnist_fft.py
+++ mnist_cnn/scripts/create_mnist_fft.py
@@ -38,24 +38,24 @@ def main(data_path, out_path, size, bundle_size, noise=False):
     )
     [
         prepare_mnist_bundles(bund, out_path, "train", pixel=size, noise=noise)
-        for bund in tqdm(bundles_train[0:1])
+        for bund in tqdm(bundles_train)
     ]
 
     print("\nCreating valid set.\n")
     bundles_evaluation = valid_x.reshape(
         int(len(valid_x) / bundle_size), bundle_size, 28, 28
     )
-    bundles_valid = bundles_evaluation[0:-2]
+    bundles_valid = bundles_evaluation[0:-10]
     [
         prepare_mnist_bundles(bund, out_path, "valid", pixel=size, noise=noise)
-        for bund in tqdm(bundles_valid[0:1])
+        for bund in tqdm(bundles_valid)
     ]
 
     print("\nCreating test set.\n")
-    bundles_test = bundles_evaluation[-2:]
+    bundles_test = bundles_evaluation[-10:]
     [
         prepare_mnist_bundles(bund, out_path, "test", pixel=size, noise=noise)
-        for bund in tqdm(bundles_test[0:1])
+        for bund in tqdm(bundles_test)
     ]
 
 
