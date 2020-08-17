189b5a07007cb0b4b9f7cafb539734170f84df40
Felix Geyer <felix.geyer@tu-dortmund.de>
2020-08-17 15:43:52 +0200

    Better criterion for Fourier exception

--- dl_framework/data.py
+++ dl_framework/data.py
@@ -108,7 +108,7 @@ class h5_dataset:
 
                 data_channel = torch.cat([data_amp, data_phase], dim=1)
         else:
-            if data.shape[-1] ** 2 < (data.shape[1]*data.shape[2]**2):
+            if data.shape[1] == 2:
                 raise ValueError("Two channeled data is used despite Fourier being False. Set Fourier to True!")
             if len(i) == 1:
                 data_channel = data.reshape(data.shape[-1] ** 2)
