f3448621fef54da5aade3627d44a048130d8998f
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-08-31 17:24:10 +0200

    add docstring, gaussian stuff

--- simulations/scripts/create_fft_images.py
+++ simulations/scripts/create_fft_images.py
@@ -1,12 +1,31 @@
-from simulations.scripts.create_mnist_fft import main as mnist_fft
+from simulations.scripts.mnist import mnist_fft
+from simulations.scripts.gaussians import simulate_gaussian_sources
 
 
 def create_fft_images(sim_conf):
-    if sim_conf['type'] == "mnist":
+    """
+    Create fft source images and save them to h5 files.
+
+    Parameters
+    ----------
+    sim_conf : dict
+        dict holding simulation parameters
+    """
+    if sim_conf["type"] == "mnist":
         mnist_fft(
             data_path=sim_conf["resource"],
             out_path=sim_conf["out_path"],
             size=sim_conf["img_size"],
             bundle_size=sim_conf["bundle_size"],
-            noise=sim_conf["noise"]
+            noise=sim_conf["noise"],
+        )
+    if sim_conf["type"] == "gaussians":
+        simulate_gaussian_sources(
+            out_path=sim_conf["out_path"],
+            num_bundles=sim_conf["num_bundles"],
+            bundle_size=sim_conf["bundle_size"],
+            img_size=sim_conf["img_size"],
+            num_comp_ext=sim_conf["num_components"],
+            num_pointlike=sim_conf["num_pointlike_gaussians"],
+            noise=sim_conf["noise"],
         )
