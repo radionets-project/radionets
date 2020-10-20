f5c20e1e6bff24a4d36f13d60da2e6989f24a38f
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-10-20 18:26:52 +0200

    delete more unused stuff

--- dl_framework/callbacks.py
+++ dl_framework/callbacks.py
@@ -9,18 +9,6 @@ from pathlib import Path
 from fastcore.foundation import L
 
 
-class CancelTrainException(Exception):
-    pass
-
-
-class CancelEpochException(Exception):
-    pass
-
-
-class CancelBatchException(Exception):
-    pass
-
-
 class TelegramLoggerCallback(Callback):
     def __init__(self, model_name):
         self.model_name = model_name
