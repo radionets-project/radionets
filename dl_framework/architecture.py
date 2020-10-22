0058ba097fad5e5baca2c3b5e45caa955b687501
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-10-22 17:24:51 +0200

    fix typo

--- dl_framework/architecture.py
+++ dl_framework/architecture.py
@@ -1,4 +1,4 @@
 from dl_framework.architectures.basics import *
 from dl_framework.architectures.unet import *
 from dl_framework.architectures.filter import *
-from dl_framework.architecture.filter_deep import *
+from dl_framework.architectures.filter_deep import *
