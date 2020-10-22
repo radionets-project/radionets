89cf708f498dae8fd66f3b7215f0886a722de4ac
Felix Geyer <felix.geyer@tu-dortmund.de>
2020-10-22 11:55:54 +0200

    Commented new stuff out

--- utilities/travis_setup.sh
+++ utilities/travis_setup.sh
@@ -9,17 +9,13 @@ export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
 if ! command -v lualatex > /dev/null; then
     echo "Texlive not installed"
     echo "Downloading texlive and installing"
-    # wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
     curl -L http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz | tar xz
     echo I | TEXLIVE_INSTALL_PREFIX=~/tmp/texlive ./install-tl-*/install-tl
-    # tar -xzf install-tl-unx.tar.gz
-    # ./install-tl-*/install-tl --profile=./utilities/texlive.profile
     echo "Finished install TexLive"
 fi
 
 echo "Now updating TexLive"
 # update texlive
-luatex
 tlmgr option -- autobackup 0
 tlmgr update --self --all --no-auto-install
 
