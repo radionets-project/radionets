e37a8d5ef087c22e0c266f339a5225d9fe575e33
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-10-26 12:44:15 +0100

    move Unet arch

--- radionets/dl_framework/architectures/basics.py
+++ radionets/dl_framework/architectures/basics.py
@@ -56,67 +56,6 @@ def small():
     return arch
 
 
-class UNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.dconv_down1 = nn.Sequential(*double_conv(1, 4))
-        self.dconv_down2 = nn.Sequential(*double_conv(4, 8))
-        self.dconv_down3 = nn.Sequential(*double_conv(8, 16))
-        self.dconv_down4 = nn.Sequential(*double_conv(16, 32))
-        self.dconv_down5 = nn.Sequential(*double_conv(32, 64))
-
-        self.maxpool = nn.MaxPool2d(2)
-        self.upsample = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
-
-        self.dconv_up4 = nn.Sequential(*double_conv(32 + 64, 32))
-        self.dconv_up3 = nn.Sequential(*double_conv(16 + 32, 16))
-        self.dconv_up2 = nn.Sequential(*double_conv(8 + 16, 8))
-        self.dconv_up1 = nn.Sequential(*double_conv(4 + 8, 4))
-
-        self.conv_last = nn.Conv2d(4, 1, 1)
-        self.flatten = Lambda(flatten)
-
-    def forward(self, x):
-        conv1 = self.dconv_down1(x)
-        x = self.maxpool(conv1)
-
-        conv2 = self.dconv_down2(x)
-        x = self.maxpool(conv2)
-
-        conv3 = self.dconv_down3(x)
-        x = self.maxpool(conv3)
-
-        conv4 = self.dconv_down4(x)
-        x = self.maxpool(conv4)
-
-        x = self.dconv_down5(x)
-
-        x = self.upsample(x)
-        x = torch.cat([x, conv4], dim=1)
-
-        x = self.dconv_up4(x)
-
-        x = self.upsample(x)
-        x = torch.cat([x, conv3], dim=1)
-
-        x = self.dconv_up3(x)
-        x = self.upsample(x)
-        x = torch.cat([x, conv2], dim=1)
-
-        x = self.dconv_up2(x)
-        x = self.upsample(x)
-        x = torch.cat([x, conv1], dim=1)
-
-        x = self.dconv_up1(x)
-
-        x = self.conv_last(x)
-
-        out = self.flatten(x)
-
-        return out
-
-
 def autoencoder():
     arch = nn.Sequential(
         *conv(1, 4, (3, 3), 2, 1),
