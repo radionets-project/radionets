28bb4d240914b61421fbf747307214436e636192
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-10-28 17:08:31 +0100

    add source plots

--- radionets/evaluation/train_inspection.py
+++ radionets/evaluation/train_inspection.py
@@ -1,6 +1,13 @@
+import click
+import torch
+import numpy as np
 from pathlib import Path
 from radionets.dl_framework.data import load_data
-from radionets.evaluation.plotting import visualize_with_fourier, plot_results
+from radionets.evaluation.plotting import (
+    visualize_with_fourier,
+    plot_results,
+    visualize_source_reconstruction,
+)
 from radionets.evaluation.utils import (
     reshape_2d,
     load_pretrained_model,
@@ -26,19 +33,19 @@ def get_prediction(conf, num_images=None, rand=False):
     return pred, img_test, img_true
 
 
-def create_inspection_plots(train_conf, num_images=3, rand=False):
-    pred, img_test, img_true = get_prediction(train_conf, num_images, rand=rand)
-    model_path = train_conf["model_path"]
+def create_inspection_plots(conf, num_images=3, rand=False):
+    pred, img_test, img_true = get_prediction(conf, num_images, rand=rand)
+    model_path = conf["model_path"]
     out_path = Path(model_path).parent / "evaluation/"
     out_path.mkdir(parents=True, exist_ok=True)
-    if train_conf["fourier"]:
+    if conf["fourier"]:
         for i in range(len(img_test)):
             visualize_with_fourier(
                 i,
                 img_test[i],
                 pred[i],
                 img_true[i],
-                amp_phase=train_conf["amp_phase"],
+                amp_phase=conf["amp_phase"],
                 out_path=out_path,
             )
     else:
@@ -49,3 +56,51 @@ def create_inspection_plots(train_conf, num_images=3, rand=False):
             out_path,
             save=True,
         )
+
+
+def create_source_plots(conf, num_images=3, rand=False):
+    # (i, real_pred, imag_pred, real_truth, imag_truth, amp_phase, out_path):
+    """
+    function for visualizing the output of a inverse fourier transform. For now, it is
+    necessary to take the absolute of the result of the inverse fourier transform,
+    because the output is complex.
+    i: current index of the loop, just used for saving
+    real_pred: real part of the prediction computed in visualize with fourier
+    imag_pred: imaginary part of the prediction computed in visualize with fourier
+    real_truth: real part of the truth computed in visualize with fourier
+    imag_truth: imaginary part of the truth computed in visualize with fourier
+    """
+    pred, img_test, img_true = get_prediction(conf, num_images, rand=rand)
+    model_path = conf["model_path"]
+    out_path = Path(model_path).parent / "evaluation"
+    out_path.mkdir(parents=True, exist_ok=True)
+
+    if not conf["fourier"]:
+        click.echo("\n This is not a fourier dataset.\n")
+
+    if conf["amp_phase"]:
+        pred = pred.numpy()
+        amp_pred = 10 ** (10 * pred[:, 0] - 10) - 1e-10
+        amp_true = 10 ** (10 * img_true[:, 0] - 10) - 1e-10
+
+        a = amp_pred * np.cos(pred[:, 1])
+        b = amp_pred * np.sin(pred[:, 1])
+        compl_pred = a + b * 1j
+
+        a = amp_true * np.cos(img_true[:, 1])
+        b = amp_true * np.sin(img_true[:, 1])
+        compl_truth = a + b * 1j
+    else:
+        compl_pred = pred[:, 0] + pred[:, 1] * 1j
+        compl_truth = img_true[:, 0] + img_true[:, 1] * 1j
+
+    # inverse fourier transformation for prediction
+    ifft_pred = np.abs(np.fft.ifft2(compl_pred))
+
+    # inverse fourier transform for truth
+    ifft_truth = np.abs(np.fft.ifft2(compl_truth))
+
+    for i, (pred, truth) in enumerate(zip(ifft_pred, ifft_truth)):
+        visualize_source_reconstruction(pred, truth, out_path, i)
+
+    return np.abs(ifft_pred), np.abs(ifft_truth)
