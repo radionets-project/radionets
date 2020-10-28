e8a3bb86ac971ebcee2114f99c6b9c8bbb9c8fb4
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-10-28 17:23:55 +0100

    add viewing angle evaluation

--- radionets/evaluation/scripts/start_evaluation.py
+++ radionets/evaluation/scripts/start_evaluation.py
@@ -4,6 +4,7 @@ from radionets.evaluation.utils import read_config
 from radionets.evaluation.train_inspection import (
     create_inspection_plots,
     create_source_plots,
+    evaluate_viewing_angle,
 )
 
 
@@ -21,7 +22,7 @@ def main(configuration_path):
     conf = toml.load(configuration_path)
     eval_conf = read_config(conf)
 
-    click.echo("\n Evaluation config:")
+    click.echo("\nEvaluation config:")
     print(eval_conf, "\n")
 
     if eval_conf["vis_pred"]:
@@ -35,3 +36,7 @@ def main(configuration_path):
         create_source_plots(eval_conf, num_images=eval_conf["num_images"], rand=False)
 
         click.echo(f"\nCreated {eval_conf['num_images']} source predictions.\n")
+
+    if eval_conf["viewing_angle"]:
+        click.echo("\nStart evaluation of viewing angles.\n")
+        evaluate_viewing_angle(eval_conf)
