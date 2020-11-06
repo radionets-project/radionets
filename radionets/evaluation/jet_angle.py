f6ce1d023d07ffce0d137f60b95b83d664bdeb9f
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-11-06 14:10:44 +0100

    warning still there

--- radionets/evaluation/jet_angle.py
+++ radionets/evaluation/jet_angle.py
@@ -79,7 +79,7 @@ def calc_jet_angle(image):
     float
         angle between the horizontal axis and the jet axis
     """
-    image = torch.tensor(image).clone().detach()
+    image = torch.tensor(image)
     img_size = image.shape[-1]
     # ignore negagive pixels, which can appear in predictions
     image[image < 0] = 0
