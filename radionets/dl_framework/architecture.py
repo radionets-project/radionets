1f5d3f4cafc700e861f716074afb34533308db21
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-11-11 11:05:32 +0100

    add test arch for lists

--- radionets/dl_framework/architecture.py
+++ radionets/dl_framework/architecture.py
@@ -2,3 +2,4 @@ from radionets.dl_framework.architectures.basics import *
 from radionets.dl_framework.architectures.unet import *
 from radionets.dl_framework.architectures.filter import *
 from radionets.dl_framework.architectures.filter_deep import *
+from radionets.dl_framework.architectures.lists import *
