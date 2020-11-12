fix scaling

radionets/dl_framework/loss_functions.py
@@ -521,7 +521,7 @@ def pos_loss(x, y):
     to solve assignment problem.
     """
     out = x.reshape(-1, 3, 2)
-    tar = y[:, 0, :, :2]
+    tar = y[:, 0, :, :2] / 63
 
     matcher = build_matcher()
     matches = matcher(out, tar)
@@ -530,6 +530,8 @@ def pos_loss(x, y):
 
     ordered = [sort(out[v], out_ord[v]) for v in range(len(out))]
     out = torch.stack(ordered)
+    # print(out[0])
+    # print(tar[0])
 
     loss = nn.MSELoss()
     loss = loss(out, tar)