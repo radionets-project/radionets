b406ae5e5dc4ce2334235f5cec3e767956e9363c
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2020-11-12 16:58:56 +0100

    add filter_deep_list to img_size archs

--- radionets/evaluation/utils.py
+++ radionets/evaluation/utils.py
@@ -168,6 +168,7 @@ def load_pretrained_model(arch_name, model_path, img_size=63):
         arch_name == "filter_deep"
         or arch_name == "filter_deep_amp"
         or arch_name == "filter_deep_phase"
+        or arch_name == "filter_deep_list"
     ):
         arch = getattr(architecture, arch_name)(img_size)
     else:
