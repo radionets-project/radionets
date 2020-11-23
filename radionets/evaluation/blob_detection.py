160f8a5c9671fc630de54ea386233385b37da7c0
Felix Geyer <felix.geyer@tu-dortmund.de>
2020-11-23 17:19:11 +0100

    Created new evaluation method for mean difference
    also added histogram for the values

--- radionets/evaluation/blob_detection.py
+++ radionets/evaluation/blob_detection.py
@@ -1,6 +1,7 @@
 from math import sqrt
 from skimage.feature import blob_log
 import torch
+import numpy as np
 
 
 def calc_blobs(ifft_pred, ifft_truth):
@@ -22,3 +23,27 @@ def calc_blobs(ifft_pred, ifft_truth):
     blobs_log_pred[:, 2] = blobs_log_pred[:, 2] * sqrt(2)
     blobs_log_truth[:, 2] = blobs_log_truth[:, 2] * sqrt(2)
     return blobs_log_pred, blobs_log_truth
+
+
+def missing_flux(pred, truth, blob_pred, blob_truth, out_path):
+    y, x, r = blob_truth
+    x_coord, y_coord = corners(y, x, r)
+    flux_truth = truth[x_coord[0]:x_coord[1], y_coord[0]:y_coord[1]]
+    flux_pred = pred[x_coord[0]:x_coord[1], y_coord[0]:y_coord[1]]
+    return flux_pred, flux_truth
+    # print((flux_truth - flux_pred).mean())
+    # fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 6), sharey=True)
+    # ax1.imshow(flux_pred)
+    # ax2.imshow(flux_truth)
+    # outpath = str(out_path) + f"/center_{i}.png"
+    # plt.savefig(outpath, bbox_inches="tight", pad_inches=0.01)
+
+
+def corners(y, x, r):
+    r = int(np.round(r))
+    x = int(x)
+    y = int(y)
+    x_coord = [x - r, x + r + 1]
+    y_coord = [y - r, y + r + 1]
+
+    return x_coord, y_coord
