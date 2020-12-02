10957e7fa4449a144b83dda739bc83f236e7f6e6
Felix Geyer <felix.geyer@tu-dortmund.de>
2020-12-02 11:09:03 +0100

    renaming and docstring

--- radionets/evaluation/contour.py
+++ radionets/evaluation/contour.py
@@ -3,12 +3,26 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 
-def area(CS1, CS2):
-    pred_x = CS1.collections[0].get_paths()[0].vertices[:, 0]
-    pred_y = CS1.collections[0].get_paths()[0].vertices[:, 1]
+def compute_area_difference(CS_pred, CS_truth):
+    """Compute the difference of the areas of truth and prediction.
 
-    truth_x = CS2.collections[0].get_paths()[0].vertices[:, 0]
-    truth_y = CS2.collections[0].get_paths()[0].vertices[:, 1]
+    Parameters
+    ----------
+    CS_pred : contour object
+        contour object of prediction
+    CS_truth : contour object
+        contour object of truth
+
+    Returns
+    -------
+    float
+        difference between area of truth and prediction
+    """
+    pred_x = CS_pred.collections[0].get_paths()[0].vertices[:, 0]
+    pred_y = CS_pred.collections[0].get_paths()[0].vertices[:, 1]
+
+    truth_x = CS_truth.collections[0].get_paths()[0].vertices[:, 0]
+    truth_y = CS_truth.collections[0].get_paths()[0].vertices[:, 1]
 
     area_pred = 0.5 * np.sum(
         pred_y[:-1] * np.diff(pred_x) - pred_x[:-1] * np.diff(pred_y)
@@ -24,16 +38,29 @@ def area(CS1, CS2):
 
 
 def area_of_contour(ifft_pred, ifft_truth):
+    """Create first contour of prediction and truth and return
+    the area difference.
+
+    Parameters
+    ----------
+    ifft_pred : ndarray
+        source image of prediction
+    ifft_truth : ndarray
+        source image of truth
+
+    Returns
+    -------
+    float
+        area differencw
+    """
     mpl.use("Agg")
 
     levels = [ifft_truth.max() * 0.1]
 
     CS1 = plt.contour(ifft_pred, levels=levels)
 
-    plt.close()
-
     CS2 = plt.contour(ifft_truth, levels=levels)
 
-    val = area(CS1, CS2)
+    val = compute_area_difference(CS1, CS2)
     mpl.rcParams.update(mpl.rcParamsDefault)
     return val
