264f87e4d461c6869817edf2113641ce791a6f54
Felix Geyer <felix.geyer@tu-dortmund.de>
2020-12-03 13:33:26 +0100

    Added bigger architecture with 8 blocks

--- radionets/dl_framework/architectures/res_exp.py
+++ radionets/dl_framework/architectures/res_exp.py
@@ -41,3 +41,45 @@ class SRResNet_small(nn.Module):
         x1 = self.symmetry_imag(x[:, 1]).reshape(-1, 1, 63, 63)
 
         return torch.cat([x0, x1], dim=1)
+
+
+class SRResNet_bigger(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.preBlock = nn.Sequential(
+            nn.Conv2d(2, 64, 9, stride=1, padding=4, groups=2), nn.PReLU()
+        )
+
+        # ResBlock 4
+        self.blocks = nn.Sequential(
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+        )
+
+        self.postBlock = nn.Sequential(
+            nn.Conv2d(64, 64, 3, stride=1, padding=1), nn.BatchNorm2d(64)
+        )
+
+        self.final = nn.Sequential(nn.Conv2d(64, 2, 9, stride=1, padding=4, groups=2),)
+
+        self.symmetry_amp = Lambda(partial(symmetry, mode="real"))
+        self.symmetry_imag = Lambda(partial(symmetry, mode="imag"))
+
+    def forward(self, x):
+        x = self.preBlock(x)
+
+        x = x + self.postBlock(self.blocks(x))
+
+        x = self.final(x)
+
+        x0 = self.symmetry_amp(x[:, 0]).reshape(-1, 1, 63, 63)
+        x1 = self.symmetry_imag(x[:, 1]).reshape(-1, 1, 63, 63)
+
+        return torch.cat([x0, x1], dim=1)
