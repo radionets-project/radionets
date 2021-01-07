51f3bd422f753c90718115380754d6b79115fcdd
Felix Geyer <felix.geyer@tu-dortmund.de>
2021-01-07 14:09:04 +0100

    Comment extra labelsizes out

--- radionets/evaluation/utils.py
+++ radionets/evaluation/utils.py
@@ -104,11 +104,11 @@ def make_axes_nice(fig, ax, im, title, phase=False, phase_diff=False):
     else:
         cbar = fig.colorbar(im, cax=cax, orientation="vertical")
 
-    cbar.set_label("Intensity / a.u.", size=16)
-    cbar.ax.tick_params(labelsize=16)
+    cbar.set_label("Intensity / a.u.")
+    # cbar.ax.tick_params(labelsize=16)
     # cbar.ax.yaxis.get_offset_text().set_fontsize(16)
     # cbar.formatter.set_powerlimits((0, 0))
-    cbar.update_ticks()
+    # cbar.update_ticks()
     if phase:
         # set ticks for colorbar
         cbar.ax.set_yticklabels([r"$-\pi$", r"$0$", r"$\pi$"])
