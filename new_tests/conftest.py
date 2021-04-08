50f86e68c58b661bad9aec51dcc47e6b3c1219b5
Felix Geyer <felix.geyer@tu-dortmund.de>
2021-04-08 15:07:58 +0200

    Reset changes

--- new_tests/conftest.py
+++ new_tests/conftest.py
@@ -1,12 +1,12 @@
-# import pytest
-# import shutil
+import pytest
+import shutil
 
 
-# @pytest.yield_fixture(autouse=True, scope='session')
-# def test_suite_cleanup_thing():
-#     yield
+@pytest.yield_fixture(autouse=True, scope='session')
+def test_suite_cleanup_thing():
+    yield
 
-#     build = "./new_tests/build/"
-#     print("Cleaning up tests.")
+    build = "./new_tests/build/"
+    print("Cleaning up tests.")
 
-#     shutil.rmtree(build)
+    shutil.rmtree(build)
