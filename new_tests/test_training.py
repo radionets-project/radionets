9c017dc5be6aeeee36621ba49383db0d4afb8940
Felix Geyer <felix.geyer@tu-dortmund.de>
2021-04-15 09:31:03 +0200

    Fix name bug

--- new_tests/test_training.py
+++ new_tests/test_training.py
@@ -3,21 +3,21 @@ import subprocess
 
 def test_lr_find():
     list_files = subprocess.run(
-        ["radionets_training", "new_tests/training_test.toml", "--mode=lr_find"]
+        ["radionets_training", "new_tests/training.toml", "--mode=lr_find"]
     )
     print("The exit code was: %d" % list_files.returncode)
     assert list_files.returncode == 0
 
 
 def test_training():
-    list_files = subprocess.run(["radionets_training", "new_tests/training_test.toml"])
+    list_files = subprocess.run(["radionets_training", "new_tests/training.toml"])
     print("The exit code was: %d" % list_files.returncode)
     assert list_files.returncode == 0
 
 
 def test_plot_loss():
     list_files = subprocess.run(
-        ["radionets_training", "new_tests/training_test.toml", "--mode=plot_loss"]
+        ["radionets_training", "new_tests/training.toml", "--mode=plot_loss"]
     )
     print("The exit code was: %d" % list_files.returncode)
     assert list_files.returncode == 0
