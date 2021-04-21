a5aaf21f23adbe7a0fa7da19c184d2d5cc427c5b
Felix Geyer <felix.geyer@tu-dortmund.de>
2021-04-21 15:43:06 +0200

    New Test

--- new_tests/test_simulation.py
+++ new_tests/test_simulation.py
@@ -1,7 +1,12 @@
-import subprocess
+# import subprocess
+from click.testing import CliRunner
 
 
 def test_simulation():
-    list_files = subprocess.run(["radionets_simulations", "new_tests/simulate.toml"])
-    print("The exit code was: %d" % list_files.returncode)
-    assert list_files.returncode == 0
+    from radionets.simulations.scripts.simulate_images import main
+
+    runner = CliRunner()
+    result = runner.invoke(main, "new_tests/simulate.toml")
+    # list_files = subprocess.run(["radionets_simulations", "new_tests/simulate.toml"])
+    # print("The exit code was: %d" % result)
+    assert result.exit_code == 0
