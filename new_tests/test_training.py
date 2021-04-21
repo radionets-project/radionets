c7518be0bfdb8c5a3a68fbef255ef52d4dc59b20
Felix Geyer <felix.geyer@tu-dortmund.de>
2021-04-21 17:20:45 +0200

    Without lr find

--- new_tests/test_training.py
+++ new_tests/test_training.py
@@ -2,11 +2,11 @@ from click.testing import CliRunner
 from radionets.dl_training.scripts.start_training import main
 
 
-def test_lr_find():
-    runner = CliRunner()
-    options = ["new_tests/training.toml", "--mode=lr_find"]
-    result = runner.invoke(main, options)
-    assert result.exit_code == 0
+# def test_lr_find():
+#     runner = CliRunner()
+#     options = ["new_tests/training.toml", "--mode=lr_find"]
+#     result = runner.invoke(main, options)
+#     assert result.exit_code == 0
 
 
 def test_training():
