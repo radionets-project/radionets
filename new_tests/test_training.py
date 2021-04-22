ebc3d2f6846ec5097debaa611661bd913caf81be
Felix Geyer <felix.geyer@tu-dortmund.de>
2021-04-22 10:14:41 +0200

    Tidy up

--- new_tests/test_training.py
+++ new_tests/test_training.py
@@ -1,15 +1,8 @@
 from click.testing import CliRunner
-from radionets.dl_training.scripts.start_training import main
-
-
-# def test_lr_find():
-#     runner = CliRunner()
-#     options = ["new_tests/training.toml", "--mode=lr_find"]
-#     result = runner.invoke(main, options)
-#     assert result.exit_code == 0
 
 
 def test_training():
+    from radionets.dl_training.scripts.start_training import main
     runner = CliRunner()
     options = ["new_tests/training.toml"]
     result = runner.invoke(main, options)
@@ -17,6 +10,7 @@ def test_training():
 
 
 def test_plot_loss():
+    from radionets.dl_training.scripts.start_training import main
     runner = CliRunner()
     options = ["new_tests/training.toml", "--mode=plot_loss"]
     result = runner.invoke(main, options)
