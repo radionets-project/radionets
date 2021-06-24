f2461456379d1397045ba65354549cb81b7d381e
Felix Geyer <felix.geyer@tu-dortmund.de>
2021-06-24 13:38:51 +0200

    Adapt tests to new get_prediction

--- new_tests/test_evaluation.py
+++ new_tests/test_evaluation.py
@@ -47,11 +47,12 @@ class TestEvaluation:
         )
         import toml
         import torch
+        from radionets.evaluation.train_inspection import get_prediction
 
         config = toml.load("./new_tests/evaluate.toml")
         conf = read_config(config)
 
-        pred, img_test, img_true = generate_images(conf)
+        pred, img_test, img_true = get_prediction(conf)
         assert str(pred.device) == "cpu"
 
         # test for uncertainty
@@ -61,12 +62,14 @@ class TestEvaluation:
             pred = torch.cat((pred_1, pred_2), dim=1)
 
         assert pred.shape == (10, 2, 63, 63)
+        assert img_test.shape == (10, 2, 63, 63)
+        assert img_true.shape == (10, 2, 63, 63)
 
         pred = pred.numpy()
         out_path = Path("./new_tests/build/test_training/evaluation/")
         out_path.mkdir(parents=True, exist_ok=True)
         save_pred(
-            str(out_path) + "/predictions_model_test.h5",
+            str(out_path) + "/predictions_model_eval.h5",
             pred,
             img_test,
             img_true,
@@ -79,7 +82,7 @@ class TestEvaluation:
         from radionets.evaluation.utils import (
             read_config,
             get_ifft,
-            save_pred,
+            read_pred,
         )
         from radionets.evaluation.contour import (
             area_of_contour,
@@ -90,9 +93,9 @@ class TestEvaluation:
         config = toml.load("./new_tests/evaluate.toml")
         conf = read_config(config)
 
-        pred, img_test, img_true = generate_images(conf)
-
-        assert str(pred.device) == "cpu"
+        pred, img_test, img_true = read_pred(
+            "./new_tests/build/test_training/evaluation/predictions_model_eval.h5"
+        )
 
         ifft_pred = get_ifft(pred, amp_phase=conf["amp_phase"])
         ifft_truth = get_ifft(img_true, amp_phase=conf["amp_phase"])
@@ -107,16 +110,6 @@ class TestEvaluation:
         assert isinstance(val, np.float64)
         assert ~np.isnan(val).any()
         assert val > 0
-
-        save_pred(
-            "./new_tests/build/test_training/evaluation/predictions_model_eval.h5",
-            pred,
-            img_test,
-            img_true,
-            "pred",
-            "img_test",
-            "img_true",
-        )
 
     def test_im_to_array_value(self):
         from radionets.evaluation.utils import read_pred
@@ -308,27 +301,3 @@ class TestEvaluation:
 
         if os.path.exists("new_tests/model/evaluation"):
             shutil.rmtree("new_tests/model/evaluation")
-
-
-def generate_images(conf):
-    from radionets.dl_framework.data import load_data
-    from radionets.evaluation.utils import get_images, eval_model, load_pretrained_model
-
-    test_ds = load_data(
-        conf["data_path"],
-        mode="test",
-        fourier=conf["fourier"],
-        source_list=conf["source_list"],
-    )
-
-    num_images = conf["num_images"]
-    rand = conf["random"]
-
-    if num_images is None:
-        num_images = len(test_ds)
-    img_test, img_true = get_images(test_ds, num_images, norm_path="none", rand=rand)
-    img_size = img_test.shape[-1]
-    model = load_pretrained_model(conf["arch_name"], conf["model_path"], img_size)
-    pred = eval_model(img_test, model, test=True)
-
-    return pred, img_test, img_true
