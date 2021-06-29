cb12ca69075f97bf375f6478abfe9b91417a65ff
Felix Geyer <felix.geyer@tu-dortmund.de>
2021-06-29 14:02:38 +0200

    Add squeeze if unsqueezed for get_ifft

--- radionets/evaluation/utils.py
+++ radionets/evaluation/utils.py
@@ -285,6 +285,8 @@ def get_ifft(array, amp_phase=False):
         compl = a + b * 1j
     else:
         compl = array[:, 0] + array[:, 1] * 1j
+    if compl.shape[0] == 1:
+        compl = compl.squeeze(0)
     return np.abs(np.fft.ifft2(compl))
 
 
