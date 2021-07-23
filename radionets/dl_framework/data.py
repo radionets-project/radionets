640b25f5436c3f762e0a3078ffe5df44db4482f1
Arne Poggenpohl <arne.poggenpohl@tu-dortmund.de>
2021-07-23 10:39:44 +0200

    make tests run again

--- radionets/dl_framework/data.py
+++ radionets/dl_framework/data.py
@@ -305,5 +305,5 @@ def load_data(data_path, mode, fourier=False, source_list=False):
     """
     bundle_paths = get_bundles(data_path)
     data = [path for path in bundle_paths if re.findall("samp_" + mode, path.name)]
-    ds = h5_dataset_segmentation(data, tar_fourier=fourier, source_list=source_list)
+    ds = h5_dataset(data, tar_fourier=fourier, source_list=source_list)
     return ds
