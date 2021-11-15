67d15e05b9499b28679326221ebecda08ebf477f
Felix Geyer <felix.geyer@tu-dortmund.de>
2021-11-15 17:29:28 +0100

    Add 16 Layer Arch without symmetry

--- radionets/dl_framework/architectures/res_exp.py
+++ radionets/dl_framework/architectures/res_exp.py
@@ -218,6 +218,57 @@ class SRResNet_bigger_16(nn.Module):
         return torch.cat([x0, x1], dim=1)
 
 
+class SRResNet_bigger_16_no_symmetry(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.preBlock = nn.Sequential(
+            nn.Conv2d(2, 64, 9, stride=1, padding=4, groups=2), nn.PReLU()
+        )
+
+        # ResBlock 16
+        self.blocks = nn.Sequential(
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+        )
+
+        self.postBlock = nn.Sequential(
+            nn.Conv2d(64, 64, 3, stride=1, padding=1, bias=False), nn.BatchNorm2d(64)
+        )
+
+        self.final = nn.Sequential(nn.Conv2d(64, 2, 9, stride=1, padding=4, groups=2),)
+
+        self.hardtanh = nn.Hardtanh(-pi, pi)
+
+    def forward(self, x):
+        s = x.shape[-1]
+
+        x = self.preBlock(x)
+
+        x = x + self.postBlock(self.blocks(x))
+
+        x = self.final(x)
+
+        x0 = x[:, 0].reshape(-1, 1, s, s)
+        x1 = self.hardtanh(x[:, 1]).reshape(-1, 1, s, s)
+
+        return torch.cat([x0, x1], dim=1)
+
+
 class SRResNet_amp(nn.Module):
     def __init__(self):
         super().__init__()
