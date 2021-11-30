bdf49f028c5983b1b434f7b1ce391eea56a8242e
Stefan Fröse <stefan.froese@tu-dortmund.de>
2021-11-30 10:33:27 +0100

    eht layout

--- radionets/simulations/layouts/layouts.py
+++ radionets/simulations/layouts/layouts.py
@@ -9,3 +9,9 @@ def vlba():
     x, y, z, _, _ = np.genfromtxt(file_dir / "vlba.txt", unpack=True)
     ant_pos = np.array([x, y, z])
     return ant_pos
+
+def eht():
+    _, x, y, z, _, _, _, _, _ = np.genfromtxt(file_dir / "eht.txt", unpack=True)
+    print(x)
+    ant_pos = np.array([x, y, z])
+    return ant_pos
