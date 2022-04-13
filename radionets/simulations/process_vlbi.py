77f74257c00fffa85a5922a38e8bea7dc82d2e9c
Stefan Fröse <stefan.froese@tu-dortmund.de>
2022-04-13 10:56:37 +0200

    gridding fix

--- radionets/simulations/process_vlbi.py
+++ radionets/simulations/process_vlbi.py
@@ -356,8 +356,8 @@ def process_eht(data_path, file, config, fov_asec):
         ph = np.angle(x*w+1j*y*w)
         u = np.array([])
         v = np.array([])
-        u = np.append(u,data['UU']*(freq))
-        v = np.append(v,data['VV']*(freq))
+        u = np.append(u,data['UU---SIN']*(freq))
+        v = np.append(v,data['VV---SIN']*(freq))
         samps = [np.append(u,-u),np.append(v,-v),np.append(ap,ap),np.append(ph,-ph)]
     
     # plt.plot(samps[0], samps[1], 'x')
@@ -406,8 +406,105 @@ def process_eht(data_path, file, config, fov_asec):
 
     # truth_fft = np.array([np.fft.fft2(np.fft.fftshift(img)) for im in img_resized])
 
-    out = data_path + "/eht_hi__startmod_weights.h5"
+    out = data_path + "/eht_hi_test_DPG_startmod.h5"
     save_fft_pair_with_response(out, samp_img, samp_img, np.expand_dims(base_mask,0), np.expand_dims(A,0))
+
+def process_eht_hist(data_path, file, config, fov_asec):
+    
+    print(f"\n Loading VLBI data set.\n")
+    configs = config
+    size = 1
+    N=64
+    with fits.open(file) as hdul:
+        baselines = hdul[0].data['Baseline']
+        
+        unique_telescopes = 8
+        unique_baselines = (unique_telescopes**2 - unique_telescopes)/2
+        freq = 229071e6#hdul[0].header[37]
+        baselines = np.append(baselines,baselines)
+    # response matrices
+    A = response(configs, N, unique_telescopes, unique_baselines, 'eht')
+
+    # samps = np.zeros((size,4,n_sampled*2))
+    print(f"\n Load subset.\n")
+
+
+    with fits.open(file) as hdul:
+        data = hdul[0].data
+        cmplx = data['DATA'] 
+        x = cmplx[...,0,0]
+        y = cmplx[...,0,1]
+        w = cmplx[...,0,2]
+        x = np.squeeze(x)
+        y = np.squeeze(y)
+        w = np.squeeze(w)
+        ap = np.sqrt(x**2+y**2)
+        ph = np.angle(x+1j*y)
+        u = np.array([])
+        v = np.array([])
+        u = np.append(u,data['UU---SIN']*(freq))
+        v = np.append(v,data['VV---SIN']*(freq))
+        samps = [np.append(u,-u),np.append(v,-v),np.append(ap,ap),np.append(ph,-ph)]
+    
+    # plt.plot(samps[0], samps[1], 'x')
+    # plt.show()
+    print(f"\n Gridding VLBI data set.\n")
+
+    # Generate Mask
+    u_0 = samps[0]
+    v_0 = samps[1]
+    mask = np.zeros((N,N,u_0.shape[0]))
+    
+    base_mask = np.zeros((N,N,int(unique_baselines)))
+    
+    fov = fov_asec*np.pi/(3600*180) # hard code #default 0.00018382
+    delta_u = 1/(fov) # with a set N this is the same as zooming in since N*delta_u can be smaller than u_max
+    binpos = np.arange(N//2+1)*delta_u
+    binsneg = -np.flip(np.arange(N//2+1)*delta_u)
+    bins = np.append(binsneg,binpos)
+    bins = np.unique(bins)
+    for i in range(N):
+        for j in range(N):
+            u_cell = (j-N/2)*delta_u
+            v_cell = (i-N/2)*delta_u
+            mask[i,j] = ((u_cell <= u_0) & (u_0 <= u_cell+delta_u)) & ((v_cell <= v_0) & (v_0 <= v_cell+delta_u))
+            
+            base = np.unique(baselines[mask[i,j].astype(bool)])
+            base_mask[i,j,:base.shape[0]] = base
+
+    import matplotlib.pyplot as plt
+    # plt.imshow(np.sum(mask, 2))
+    # plt.show()
+    amp_cal,_,_,_ = plt.hist2d(samps[1],samps[0],bins=bins, weights=np.append(ap,ap))
+    phase_cal,_,_,_ = plt.hist2d(samps[1],samps[0],bins=bins, weights=np.append(ph,-ph))
+    points_cal,_,_,_ = plt.hist2d(samps[1],samps[0],bins=bins)
+    points_cal[points_cal==0]=1
+    amp_cal = amp_cal/points_cal
+    phase_cal = phase_cal/points_cal
+
+    samp_img = np.zeros((size,2,N,N))
+    print(mask.shape)
+    print(samps[2].shape)
+    samp_img[0,0] = amp_cal
+    samp_img[0,0] = (np.log10(samp_img[0,0] + 1e-10) / 10) + 1 
+    samp_img[0,1] = phase_cal
+    plt.imshow(points_cal)
+    plt.colorbar()
+    plt.show()
+    plt.imshow(samp_img[0,0])
+    plt.colorbar()
+    plt.show()
+    plt.imshow(samp_img[0,1])
+    plt.colorbar()
+    plt.show()
+
+
+    # truth_fft = np.array([np.fft.fft2(np.fft.fftshift(img)) for im in img_resized])
+
+    out = data_path + "/eht_hi_test_DPG.h5"
+    save_fft_pair_with_response(out, samp_img, samp_img, np.expand_dims(base_mask,0), np.expand_dims(A,0))
+
+
 
 
 def process_data_dirty_model_noisy(data_path, freq, n_positions, fov_asec, layout):
@@ -415,11 +512,12 @@ def process_data_dirty_model_noisy(data_path, freq, n_positions, fov_asec, layou
     print(f"\n Loading VLBI data set.\n")
     bundles = dt.get_bundles(data_path)
     freq = freq*10**6 # mhz hard code #eht 227297
-    uvfits = dt.get_bundles(bundles[2])
-    imgs = dt.get_bundles(bundles[4])
-    configs = dt.get_bundles(bundles[1])
-    uv_srt = natsorted(uvfits, alg=ns.PATH)
-    img_srt = natsorted(imgs, alg=ns.PATH)
+    uvfits = dt.get_bundles(bundles[3])
+    imgs = dt.get_bundles(bundles[2])
+    configs = dt.get_bundles(bundles[0])
+    uv_srt = natsorted(uvfits, alg=ns.PATH)[50000:]
+    img_srt = natsorted(imgs, alg=ns.PATH)[50000:]
+    configs = natsorted(configs, alg=ns.PATH)[50:]
     size = 1000
     for p in tqdm(range(n_positions)):
         N = 64 # hard code
@@ -433,7 +531,7 @@ def process_data_dirty_model_noisy(data_path, freq, n_positions, fov_asec, layou
         # response matrices
         A = response(configs[p], N, unique_telescopes, unique_baselines, layout)
 
-        img = np.zeros((size,128,128))
+        img = np.zeros((size,256,256))
         samps = np.zeros((size,4,n_sampled*2))
         print(f"\n Load subset.\n")
         for i in np.arange(p*1000, p*1000+1000):
@@ -531,9 +629,9 @@ def process_data_dirty_model_noisy(data_path, freq, n_positions, fov_asec, layou
         truth_fft = np.fft.fftshift(np.fft.fft2(np.fft.fftshift(img_resized, axes=(1,2)), axes=(1,2)), axes=(1,2))
         fft_scaled_truth = prepare_fft_images(truth_fft, True, False)
 
-        out = data_path + "/h5/samp_train"+ str(p) +".h5"
+        out = data_path + "/h5/bh/samp_train"+ str(p) +".h5"
         save_fft_pair_with_response(out, samp_img[:800], fft_scaled_truth[:800], np.expand_dims(base_mask,0), np.expand_dims(A,0))
-        out = data_path + "/h5/samp_valid"+ str(p) + ".h5"
+        out = data_path + "/h5/bh/samp_valid"+ str(p) + ".h5"
         save_fft_pair_with_response(out, samp_img[800:], fft_scaled_truth[800:], np.expand_dims(base_mask,0), np.expand_dims(A,0))
 
 def process_data_dirty_model_noisy_pointSource(data_path, freq, n_positions, fov_asec, layout):
