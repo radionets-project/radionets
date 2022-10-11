14e0f15038707ee5aea9aec0726bed02cf535041
Felix Geyer <felix.geyer@tu-dortmund.de>
2022-10-11 15:29:05 +0200

    Delete imports

--- radionets/dl_framework/architecture.py
+++ radionets/dl_framework/architecture.py
@@ -1,7 +1,3 @@
-from radionets.dl_framework.architectures.basics import *
-from radionets.dl_framework.architectures.unet import *
-from radionets.dl_framework.architectures.filter_deep import *
-from radionets.dl_framework.architectures.superRes import *
 from radionets.dl_framework.architectures.res_exp import *
 from radionets.dl_framework.architectures.lists import *
 from radionets.dl_framework.architectures.unc_archs import *
