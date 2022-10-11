10219d5c952e9930ad06c7040345a00c74266bfb
Felix Geyer <felix.geyer@tu-dortmund.de>
2022-10-11 15:44:50 +0200

    Bump up version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="radionets",
-    version="0.1.13",
+    version="0.1.14",
     description="Imaging radio interferometric data with neural networks",
     url="https://github.com/Kevin2/radionets",
     author="Kevin Schmidt, Felix Geyer",
