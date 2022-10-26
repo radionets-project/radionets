821a8de8bc34b308b46d85e3254e373ae5288491
Felix Geyer <felix.geyer@tu-dortmund.de>
2022-10-26 15:55:15 +0200

    Delete unsued arch and update arch with 16 res blocks

--- radionets/dl_framework/architectures/res_exp.py
+++ radionets/dl_framework/architectures/res_exp.py
@@ -72,54 +72,6 @@ class SRResNet_shuffle(nn.Module):
         return torch.cat([x0, x1], dim=1)
 
 
-class SRResNet_bigger(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.preBlock = nn.Sequential(
-            nn.Conv2d(2, 64, 9, stride=1, padding=4, groups=2), nn.PReLU()
-        )
-
-        # ResBlock 8
-        self.blocks = nn.Sequential(
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-        )
-
-        self.postBlock = nn.Sequential(
-            nn.Conv2d(64, 64, 3, stride=1, padding=1, bias=False), nn.BatchNorm2d(64)
-        )
-
-        self.final = nn.Sequential(
-            nn.Conv2d(64, 2, 9, stride=1, padding=4, groups=2),
-        )
-
-        self.symmetry_amp = Lambda(partial(symmetry, mode="real"))
-        self.symmetry_imag = Lambda(partial(symmetry, mode="imag"))
-        self.hardtanh = nn.Hardtanh(-pi, pi)
-
-    def forward(self, x):
-        s = x.shape[-1]
-
-        x = self.preBlock(x)
-
-        x = x + self.postBlock(self.blocks(x))
-
-        x = self.final(x)
-
-        x0 = self.symmetry_amp(x[:, 0]).reshape(-1, 1, s, s)
-        x1 = self.hardtanh(x[:, 1]).reshape(-1, 1, s, s)
-        x1 = self.symmetry_imag(x1).reshape(-1, 1, s, s)
-
-        return torch.cat([x0, x1], dim=1)
-
-
 class SRResNet(nn.Module):
     def __init__(self):
         super().__init__()
@@ -167,7 +119,7 @@ class SRResNet(nn.Module):
         return torch.cat([x0, x1], dim=1)
 
 
-class SRResNet_bigger_16(nn.Module):
+class SRResNet_16(nn.Module):
     def __init__(self):
         super().__init__()
 
@@ -203,8 +155,8 @@ class SRResNet_bigger_16(nn.Module):
             nn.Conv2d(64, 2, 9, stride=1, padding=4, groups=2),
         )
 
-        self.symmetry_amp = Lambda(partial(symmetry, mode="real"))
-        self.symmetry_imag = Lambda(partial(symmetry, mode="imag"))
+        self.hardtanh = nn.Hardtanh(-pi, pi)
+        self.relu = nn.ReLU()
 
     def forward(self, x):
         s = x.shape[-1]
@@ -215,8 +167,9 @@ class SRResNet_bigger_16(nn.Module):
 
         x = self.final(x)
 
-        x0 = self.symmetry_amp(x[:, 0]).reshape(-1, 1, s, s)
-        x1 = self.symmetry_imag(x[:, 1]).reshape(-1, 1, s, s)
+        x0 = x[:, 0].reshape(-1, 1, s, s)
+        x0 = self.relu(x0)
+        x1 = self.hardtanh(x[:, 1]).reshape(-1, 1, s, s)
 
         return torch.cat([x0, x1], dim=1)
 
