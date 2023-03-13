d46d6beb61f491467ca90e54ca54af4b2a87ee58
Arne Poggenpohl <arne.poggenpohl@tu-dortmund.de>
2023-03-13 17:10:26 +0100

    Adjust GMM clustering

--- radionets/dl_framework/clustering.py
+++ radionets/dl_framework/clustering.py
@@ -33,7 +33,7 @@ def bgmmClustering(data, n_components: int = 10, n_init: int = 1):
 
 
 def gmmClustering(
-    data, n_components: int = 10, n_init: int = 1, score_type: str = None
+    data, n_components: int = 10, n_init: int = 3, score_type: str = "BIC"
 ):
     """Use Gaussian Mixture Model for clustering data. The number of components
     is determined by the BIC or AIC score.
@@ -54,32 +54,32 @@ def gmmClustering(
     best_gmm:
         best fitted Gaussian Mixture Model
     """
+    if len(data) < n_components:
+        n_components = len(data)
+
+    best_gmm = None
     lowest_score = np.infty
     score = []
-    if score_type:
-        for i in range(n_components):
-            gmm = GaussianMixture(
-                n_components=i + 1, n_init=n_init, init_params="k-means++"
-            ).fit(data)
-
-            if score_type == "AIC":
-                score.append(gmm.aic(data))
-                if score[-1] < lowest_score and score[-1] > 0:
-                    lowest_score = score[-1]
-                    best_gmm = gmm
-            elif score_type == "BIC":
-                score.append(gmm.bic(data))
-                if score[-1] < lowest_score:
-                    lowest_score = score[-1]
-                    best_gmm = gmm
-    else:
+    for i in range(n_components):
         gmm = GaussianMixture(
-            n_components=n_components, n_init=n_init, init_params="k-means++"
+            n_components=i + 1, n_init=n_init, init_params="k-means++"
         ).fit(data)
+
+        if score_type == "BIC":
+            score.append(gmm.bic(data))
+            if score[-1] < lowest_score and score[-1] > 0:
+                lowest_score = score[-1]
+                best_gmm = gmm
+        elif score_type == "AIC":
+            score.append(gmm.aic(data))
+            if score[-1] < lowest_score:
+                lowest_score = score[-1]
+                best_gmm = gmm
+
     return best_gmm
 
 
-def spectralClustering(data, n_components: int = None, eig_thres: float = 0.99):
+def spectralClustering(data, n_components: int = None, eig_thres: float = 0.98):
     """Use Spectral Clustering for clustering data.
 
     References:
