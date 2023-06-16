4e04f8cbb73c5d412a87e03e1cfd0fafeae8d82c
Felix Geyer <felix.geyer@tu-dortmund.de>
2023-06-16 14:21:04 +0200

    Change to org SRResNet

--- radionets/dl_framework/architectures/res_exp.py
+++ radionets/dl_framework/architectures/res_exp.py
@@ -152,6 +152,56 @@ class SRResNet_16(nn.Module):
 
         self.final = nn.Sequential(nn.Conv2d(64, 4, 9, stride=1, padding=4, groups=2))
 
+    def forward(self, x):
+        s = x.shape[-1]
+
+        x = self.preBlock(x)
+
+        x = x + self.postBlock(self.blocks(x))
+
+        x = self.final(x)
+
+        x0 = x[:, 0].reshape(-1, 1, s // 2 + 1, s)
+        x1 = x[:, 1].reshape(-1, 1, s // 2 + 1, s)
+
+        return torch.cat([x0, x1], dim=1)
+
+
+class SRResNet_16_unc(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.preBlock = nn.Sequential(
+            nn.Conv2d(2, 64, 9, stride=1, padding=4, groups=2), nn.PReLU()
+        )
+
+        # ResBlock 16
+        self.blocks = nn.Sequential(
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+            SRBlock(64, 64),
+        )
+
+        self.postBlock = nn.Sequential(
+            nn.Conv2d(64, 64, 3, stride=1, padding=1, bias=False),
+            nn.InstanceNorm2d(64),
+        )
+
+        self.final = nn.Sequential(nn.Conv2d(64, 4, 9, stride=1, padding=4, groups=2))
+
         self.hardtanh = nn.Hardtanh(-pi, pi)
         self.relu = nn.ReLU()
         self.elu = GeneralELU(add=+(1 + 1e-3))
