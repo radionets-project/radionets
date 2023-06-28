0054a93853074e5cb696415cc907d9849c9e3233
Felix Geyer <felix.geyer@tu-dortmund.de>
2023-06-28 12:44:51 +0000

    Delete unused architecture

--- radionets/dl_framework/architectures/res_exp.py
+++ radionets/dl_framework/architectures/res_exp.py
@@ -1,73 +1,9 @@
-from functools import partial
 from math import pi
 
 import torch
 from torch import nn
 
-from radionets.dl_framework.model import GeneralELU, Lambda, SRBlock, symmetry
-
-
-class SRResNet_shuffle(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.preBlock = nn.Sequential(
-            nn.Conv2d(2, 64, 9, stride=1, padding=4, groups=2), nn.PReLU()
-        )
-
-        # ResBlock 14
-        self.blocks = nn.Sequential(
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-            SRBlock(64, 64),
-        )
-
-        self.postBlock = nn.Sequential(
-            nn.Conv2d(64, 64, 3, stride=1, padding=1, bias=False), nn.BatchNorm2d(64)
-        )
-
-        self.shuffle = nn.Sequential(
-            nn.Conv2d(64, 252, 3, stride=1, padding=1, bias=True),
-            nn.PixelShuffle(3),
-            nn.PReLU(),
-        )
-
-        self.final = nn.Sequential(
-            nn.Conv2d(28, 2, 9, stride=1, padding=4, groups=2),
-        )
-
-        self.symmetry_amp = Lambda(partial(symmetry, mode="real"))
-        self.symmetry_imag = Lambda(partial(symmetry, mode="imag"))
-        self.hardtanh = nn.Hardtanh(-pi, pi)
-
-    def forward(self, x):
-
-        x = self.preBlock(x)
-
-        x = x + self.postBlock(self.blocks(x))
-
-        x = self.shuffle(x)
-
-        x = self.final(x)
-
-        s = x.shape[-1]
-
-        x0 = self.symmetry_amp(x[:, 0]).reshape(-1, 1, s, s)
-        x1 = self.symmetry_imag(x[:, 1]).reshape(-1, 1, s, s)
-        x1 = self.hardtanh(x1)
-
-        return torch.cat([x0, x1], dim=1)
+from radionets.dl_framework.model import GeneralELU, SRBlock
 
 
 class SRResNet(nn.Module):
