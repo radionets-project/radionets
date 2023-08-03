8fabd891889353b612eec4065af912fe17bd18a0
Felix Geyer <felix.geyer@tu-dortmund.de>
2023-08-03 09:05:24 +0200

    Create MANIFEST.in

--- radionets/simulations/layouts/layouts.py
+++ radionets/simulations/layouts/layouts.py
@@ -6,6 +6,6 @@ file_dir = Path(__file__).parent.resolve()
 
 
 def vlba():
-    x, y, z, _, _ = np.genfromtxt("radionets/simulations/layouts/vlba.txt", unpack=True)
+    x, y, z, _, _ = np.genfromtxt(file_dir / "vlba.txt", unpack=True)
     ant_pos = np.array([x, y, z])
     return ant_pos
