acdd52f1e53d206a6532d9af919281ed3294796c
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2024-06-24 12:21:05 +0200

    add neural int arch

--- radionets/dl_framework/architecture.py
+++ radionets/dl_framework/architecture.py
@@ -1,2 +1,3 @@
+from radionets.dl_framework.architectures.neural_int import *
 from radionets.dl_framework.architectures.res_exp import *
 from radionets.dl_framework.architectures.unc_archs import *
