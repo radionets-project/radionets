2faa4989cae0c71b2d6aaeba80b983a01a138d8a
Anno Knierim <anno.knierim@tu-dortmund.de>
2025-07-04 17:57:55 +0200

    Restructure architecture
    - Move network blocks from ``radionets.dl_framework.model`` to separate
      ``blocks`` submodule in ``architecture``
    - Use base classes to inherit from for architectures and blocks
    - Use move GeneralELU, GeneralReLU, and Lambda from ``radionets.dl_framework.model``
      to separate submodule ``activation`` in ``architecture``
    - Update imports

--- radionets/dl_framework/architecture/__init__.py
+++ radionets/dl_framework/architecture/__init__.py
@@ -1,8 +1,22 @@
-from .res_exp import SRResNet, SRResNet_34, SRResNet_34_unc, SRResNet_34_unc_no_grad
+from .activation import GeneralELU, GeneralReLU, Lambda
+from .archs import (
+    SRResNet,
+    SRResNet_18,
+    SRResNet_34,
+    SRResNet_34_unc,
+    SRResNet_34_unc_no_grad,
+)
+from .blocks import BaseBlock, SRBlock
 from .unc_archs import Uncertainty, UncertaintyWrapper
 
 __all__ = [
+    "BaseBlock",
+    "GeneralELU",
+    "GeneralReLU",
+    "Lambda",
+    "SRBlock",
     "SRResNet",
+    "SRResNet_18",
     "SRResNet_34",
     "SRResNet_34_unc",
     "SRResNet_34_unc_no_grad",
--- radionets/dl_framework/architecture/unc_archs.py
+++ radionets/dl_framework/architecture/unc_archs.py
@@ -1,8 +1,8 @@
 import torch
 from torch import nn
+from torch.nn.modules.utils import _pair
 
-from radionets.dl_framework.architecture import SRResNet_34
-from radionets.dl_framework.model import GeneralELU, LocallyConnected2d
+from radionets.dl_framework.architecture import GeneralELU, SRResNet_34
 
 
 class Uncertainty(nn.Module):
@@ -78,3 +78,40 @@ class UncertaintyWrapper(nn.Module):
         )
 
         return val
+
+
+class LocallyConnected2d(nn.Module):
+    def __init__(
+        self, in_channels, out_channels, output_size, kernel_size, stride, bias=False
+    ):
+        super(LocallyConnected2d, self).__init__()
+        self.weight = nn.Parameter(
+            torch.randn(
+                1,
+                out_channels,
+                in_channels,
+                output_size[0],
+                output_size[1],
+                kernel_size**2,
+            )
+        )
+        if bias:
+            self.bias = nn.Parameter(
+                torch.randn(1, out_channels, output_size[0], output_size[1])
+            )
+        else:
+            self.register_parameter("bias", None)
+        self.kernel_size = _pair(kernel_size)
+        self.stride = _pair(stride)
+
+    def forward(self, x):
+        _, c, h, w = x.size()
+        kh, kw = self.kernel_size
+        dh, dw = self.stride
+        x = x.unfold(2, kh, dh).unfold(3, kw, dw)
+        x = x.contiguous().view(*x.size()[:-2], -1)
+        # Sum in in_channel and kernel_size dims
+        out = (x.unsqueeze(1) * self.weight).sum([2, -1])
+        if self.bias is not None:
+            out += self.bias
+        return out
