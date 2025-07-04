63a5cb8e67866ab250d91dc653e53dcd11c454dd
Anno Knierim <anno.knierim@tu-dortmund.de>
2025-07-04 22:29:43 +0200

    Remove radionets from root dir

--- radionets/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-raionets - Deep Learning-based imaging in radio interferometry
-
-Licensed under a MIT style license - see LICENSE
-"""
-
-from .version import __version__
-
-__all__ = ["__version__"]
