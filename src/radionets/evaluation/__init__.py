b73cc4a24fa5285130d9db11b2de380b2b8cdcb6
Anno Knierim <anno.knierim@tu-dortmund.de>
2026-03-30 09:25:57 +0200

    refactor: add eval functions to module init

---
+++ src/radionets/evaluation/__init__.py
@@ -0,0 +1,16 @@
+from .contour import eval_area, eval_intensity, intensity_ratio, source_area_ratio
+from .dynamic_range import dynamic_range, eval_dynamic_range
+from .feature import eval_mean_difference
+from .jet_angle import eval_jet_angle
+
+__all__ = [
+    "dynamic_range",
+    "eval_area",
+    "eval_dynamic_range",
+    "eval_intensity",
+    "eval_jet_angle",
+    "eval_mean_difference",
+    "intensity_ratio",
+    "intensity_ratio",
+    "source_area_ratio",
+]
