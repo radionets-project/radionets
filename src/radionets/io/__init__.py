f20f920f415d845f4298bfc96b35636e759e705c
Anno Knierim <anno.knierim@tu-dortmund.de>
2026-03-30 09:37:11 +0200

    chore: add EvalConfig to io module init

--- src/radionets/io/__init__.py
+++ src/radionets/io/__init__.py
@@ -1,4 +1,5 @@
 from .data import H5DataModule, WebDatasetModule
+from .eval_config import EvalConfig
 from .train_config import TrainConfig
 
-__all__ = ["TrainConfig", "H5DataModule", "WebDatasetModule"]
+__all__ = ["EvalConfig", "TrainConfig", "H5DataModule", "WebDatasetModule"]
