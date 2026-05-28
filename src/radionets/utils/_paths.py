cb70827ca77ce3c2fcefd02623c7f752639a3496
Anno Knierim <anno.knierim@tu-dortmund.de>
2026-05-28 17:36:10 +0200

    fix: improve path check in radionets.utils._paths

--- src/radionets/utils/_paths.py
+++ src/radionets/utils/_paths.py
@@ -1,13 +1,14 @@
 def _validate_pre_model_path(train_config):
-    if not train_config.paths.pre_model:
+    checkpoint = train_config.paths.checkpoint
+    if not checkpoint:
         raise ValueError(
-            f"'pre_model' path is {train_config.paths.pre_model} "
+            f"'pre_model' path is {checkpoint} "
             "even though testing mode was started. Please make sure "
             "you provide a valid path to a model checkpoint file (.ckpt) "
             "in your configuration."
         )
-    if not train_config.paths.pre_model.is_file():
+    if not checkpoint.is_file():
         raise ValueError(
-            f"'pre_model' path is {train_config.paths.pre_model}, "
+            f"'pre_model' path is {checkpoint}, "
             "but not a valid path to a model checkpoint file (.ckpt)."
         )
