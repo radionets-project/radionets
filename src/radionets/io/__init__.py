c7730ea0e0919a1c64c578cbef5e0ae610fa3319
Anno Knierim <anno.knierim@tu-dortmund.de>
2026-06-16 14:41:38 +0000

    feat: add inference config to io module import

--- src/radionets/io/__init__.py
+++ src/radionets/io/__init__.py
@@ -1,5 +1,12 @@
 from .data import H5DataModule, WebDatasetModule
 from .eval_config import EvalConfig
+from .inference_config import InferenceConfig
 from .train_config import TrainConfig
 
-__all__ = ["EvalConfig", "TrainConfig", "H5DataModule", "WebDatasetModule"]
+__all__ = [
+    "EvalConfig",
+    "InferenceConfig",
+    "TrainConfig",
+    "H5DataModule",
+    "WebDatasetModule",
+]
