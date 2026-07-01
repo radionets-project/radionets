2c7f0d1a449a1a30b36e49d55e03812fc9ffb1c4
Anno Knierim <anno.knierim@tu-dortmund.de>
2026-07-01 06:59:28 +0000

    feat: add PlottingConfig to module init

--- src/radionets/io/__init__.py
+++ src/radionets/io/__init__.py
@@ -1,12 +1,14 @@
 from .data import H5DataModule, WebDatasetModule
 from .eval_config import EvalConfig
 from .inference_config import InferenceConfig
+from .plotting_config import PlottingConfig
 from .train_config import TrainConfig
 
 __all__ = [
     "EvalConfig",
-    "InferenceConfig",
-    "TrainConfig",
     "H5DataModule",
+    "InferenceConfig",
+    "PlottingConfig",
+    "TrainConfig",
     "WebDatasetModule",
 ]
